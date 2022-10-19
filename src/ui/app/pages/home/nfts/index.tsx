09753ec8ac6e216f9d6408a0cc57b8936e44ea9d
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-10-19 10:35:13 -0500

    Fix styling for empty state for NFT page

--- src/ui/app/pages/home/nfts/index.tsx
+++ src/ui/app/pages/home/nfts/index.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+import { SparklesIcon } from '@heroicons/react/24/outline';
 import { hasPublicTransfer } from '@mysten/sui.js';
 import { Link } from 'react-router-dom';
 
@@ -8,8 +9,12 @@ import PageTitle from '_app/shared/page-title';
 import NFTdisplay from '_components/nft-display';
 import { useAppSelector } from '_hooks';
 import { accountNftsSelector } from '_redux/slices/account';
+import { LinkType } from '_src/enums/LinkType';
 import { NFT_EXPERIMENT_LINK } from '_src/shared/constants';
 import Icon, { SuiIcons } from '_src/ui/app/components/icon';
+import Body from '_src/ui/app/shared/typography/Body';
+import EthosLink from '_src/ui/app/shared/typography/EthosLink';
+import Subheader from '_src/ui/app/shared/typography/Subheader';
 
 import st from './NFTPage.module.scss';
 
@@ -19,23 +24,17 @@ function NftsPage() {
     return (
         <div>
             {nfts.length <= 0 ? (
-                <div className="text-center my-auto text-gray-900 dark:text-white">
-                    <Icon className="text-4xl " icon={SuiIcons.Nfts} />
-                    <h3 className="text-lg leading-6 font-medium">
-                        No NFTs yet
-                    </h3>
-                    <div className="mt-2">
-                        <p>
-                            <a
-                                href={NFT_EXPERIMENT_LINK}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-sm text-purple-700 hover:text-purple-800 dark:text-violet-400 dark:hover:text-violet-300"
-                            >
-                                Explore creating your own digital asset →
-                            </a>
-                        </p>
-                    </div>
+                <div className="pt-24 text-center flex flex-col gap-2">
+                    <SparklesIcon className="h-6 w-6 mx-auto" />
+                    <Subheader as="h3">No NFTs yet</Subheader>
+                    <Body>
+                        <EthosLink
+                            to={NFT_EXPERIMENT_LINK}
+                            type={LinkType.External}
+                        >
+                            Explore creating your own digital asset →
+                        </EthosLink>
+                    </Body>
                 </div>
             ) : (
                 <>
