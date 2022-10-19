ffb301ed7ea73864730bf0f183e8b73a6d858f3d
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-10-19 10:40:41 -0500

    Fix styling for txn history empty state

--- src/ui/app/pages/home/transactions/index.tsx
+++ src/ui/app/pages/home/transactions/index.tsx
@@ -12,6 +12,11 @@ import Loading from '_src/ui/app/components/loading';
 import type { TxResultState } from '_redux/slices/txresults';
 
 import st from './Transactions.module.scss';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { LinkType } from '_src/enums/LinkType';
+import Body from '_src/ui/app/shared/typography/Body';
+import EthosLink from '_src/ui/app/shared/typography/EthosLink';
+import Subheader from '_src/ui/app/shared/typography/Subheader';
 
 function TransactionsPage() {
     const dispatch = useAppDispatch();
@@ -24,60 +29,31 @@ function TransactionsPage() {
         dispatch(getTransactionsByAddress()).unwrap();
     }, [dispatch]);
 
-    return txByAddress && txByAddress.length ? (
-        <div className={st.container}>
-            <PageTitle title="Activity" className={st.pageTitle} />
-            <Content>
-                <section className={st.txContent}>
-                    {txByAddress.map((txn) => (
-                        <TransactionCard txn={txn} key={txn.txId} />
-                    ))}
-                </section>
-            </Content>
-        </div>
-    ) : (
-        <div className={st.container}>
-            <div className="text-center my-auto text-gray-900 dark:text-white">
-                <Loading loading={loading} className="">
-                    <div className="w-full">
-                        {/* Tailwind hero icon: */}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-8 h-8 mx-auto mb-2"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                            />
-                        </svg>
-                    </div>
-                    <h3 className="text-lg leading-6 font-medium">
-                        No transactions yet
-                    </h3>
-                    <div className="mt-2">
-                        <p className="text-sm text-gray-500 dark:text-gray-400">
-                            Get set up with DevNet Sui tokens to send some
-                            coins.
-                        </p>
-                        <p className="mt-2">
-                            <a
-                                href="https://docs.sui.io/build/devnet"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-sm text-purple-700 hover:text-purple-800 dark:text-violet-400 dark:hover:text-violet-300"
-                            >
-                                Learn how →
-                            </a>
-                        </p>
-                    </div>
-                </Loading>
-            </div>
-        </div>
+    return (
+        <Loading loading={loading}>
+            {txByAddress && txByAddress.length ? (
+                <div className={st.container}>
+                    <PageTitle title="Activity" className={st.pageTitle} />
+                    <Content>
+                        <section className={st.txContent}>
+                            {txByAddress.map((txn) => (
+                                <TransactionCard txn={txn} key={txn.txId} />
+                            ))}
+                        </section>
+                    </Content>
+                </div>
+            ) : (
+                <div className="pt-24 text-center flex flex-col gap-2">
+                    <MagnifyingGlassIcon className="h-6 w-6 mx-auto" />
+                    <Subheader as="h3">No transactions yet</Subheader>
+                    <Body>
+                        <EthosLink to={''} type={LinkType.External}>
+                            Get DevNet Sui tokens →
+                        </EthosLink>
+                    </Body>
+                </div>
+            )}
+        </Loading>
     );
 }
 
