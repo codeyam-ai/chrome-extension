c8cd6a512c28dd7e983310f01abdde48c255a573
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-10-20 12:24:56 -0500

    Add cursor:pointer to checkbox

--- src/ui/app/shared/inputs/Checkbox.tsx
+++ src/ui/app/shared/inputs/Checkbox.tsx
@@ -30,12 +30,12 @@ const Checkbox = ({
                 type="checkbox"
                 onChange={onChange}
                 checked={checked}
-                className="rounded h-5 w-5 bg-ethos-light-background-default dark:bg-ethos-dark-background-default border checked:bg-ethos-light-primary-light dark:checked:bg-ethos-dark-primary-dark checked:text-ethos-light-primary-light dark:checked:text-ethos-dark-primary-dark border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke focus:ring-0 focus:border-2 focus:border-ethos-light-primary-light focus:dark:border-ethos-dark-primary-dark focus:shadow-ethos-light-stroke-focused dark:focus:shadow-ethos-dark-stroke-focused"
+                className="rounded h-5 w-5 cursor-pointer bg-ethos-light-background-default dark:bg-ethos-dark-background-default border checked:bg-ethos-light-primary-light dark:checked:bg-ethos-dark-primary-dark checked:text-ethos-light-primary-light dark:checked:text-ethos-dark-primary-dark border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke focus:ring-0 focus:border-2 focus:border-ethos-light-primary-light focus:dark:border-ethos-dark-primary-dark focus:shadow-ethos-light-stroke-focused dark:focus:shadow-ethos-dark-stroke-focused"
                 {...reactProps}
             />
             <label
                 htmlFor={id}
-                className="ml-2 font-weight-ethos-body text-size-ethos-body leading-line-height-ethos-body tracking-letter-spacing-ethos-body"
+                className="ml-2 cursor-pointer font-weight-ethos-body text-size-ethos-body leading-line-height-ethos-body tracking-letter-spacing-ethos-body"
                 id={`${id}-description`}
             >
                 {label}
