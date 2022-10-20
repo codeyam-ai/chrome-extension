e574d753124ecae4a79fe1d8816645fd08e7a7b5
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-10-20 10:53:40 -0500

    Fix scroll issue

--- src/ui/app/shared/layouts/PageScrollView.tsx
+++ src/ui/app/shared/layouts/PageScrollView.tsx
@@ -3,15 +3,7 @@ interface PageScrollViewProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const PageScrollView = ({ heightInPx, children }: PageScrollViewProps) => {
-    return (
-        <div
-            className={`h-[${
-                heightInPx ? heightInPx.toString() : '290'
-            }px] overflow-y-scroll`}
-        >
-            {children}
-        </div>
-    );
+    return <div className="h-[290px] overflow-scroll">{children}</div>;
 };
 
 export default PageScrollView;
