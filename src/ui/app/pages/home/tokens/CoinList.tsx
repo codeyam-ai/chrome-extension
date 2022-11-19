38cc4a662424fffbe302199d3ee31ae7c63b5a2d
Jared Cosulich <jared.cosulich@gmail.com>
2022-11-19 10:21:07 -0500

    improving coin list

---
+++ src/ui/app/pages/home/tokens/CoinList.tsx
@@ -0,0 +1,37 @@
+import CoinBalance from './CoinBalance';
+import Loading from '_src/ui/app/components/loading';
+
+const CoinList = ({ balances }: { balances: Record<string, bigint> }) => {
+    return (
+        <div className="text-left px-6">
+            <div className="text-lg">Coins</div>
+            <Loading
+                className="py-3 flex justify-center items-center"
+                loading={!balances || Object.keys(balances).length === 0}
+            >
+                {Object.keys(balances).map((type: string) => {
+                    const balance = balances[type];
+                    return (
+                        <CoinBalance type={type} balance={balance} key={type} />
+                    );
+                })}
+            </Loading>
+        </div>
+    );
+};
+
+export default CoinList;
+
+// otherCoinTypes.map((aCoinType) => {
+//     const aCoinBalance = balances[aCoinType];
+//     return (
+//         <>
+//             <div className="">OTHER COINS</div>
+//             <CoinBalance
+//                 type={aCoinType}
+//                 balance={aCoinBalance}
+//                 key={aCoinType}
+//             />
+//         </>
+//     );
+// });
