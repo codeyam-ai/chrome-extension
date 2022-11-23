ecdeea04d0debd9990d186fbe117fc7608b6e16a
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-11-22 18:00:07 -0600

    Add Segmented Control component

--- src/ui/app/components/settings-menu/subpages/ThemePage.tsx
+++ src/ui/app/components/settings-menu/subpages/ThemePage.tsx
@@ -1,5 +1,49 @@
+import {
+    ComputerDesktopIcon,
+    MoonIcon,
+    SunIcon,
+} from '@heroicons/react/24/solid';
+import SegmentedControl, {
+    SegmentedControlItem,
+} from '_src/ui/app/shared/inputs/SegmentedControl';
+import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
+import ContentBlock from '_src/ui/app/shared/typography/ContentBlock';
+import Header from '_src/ui/app/shared/typography/Header';
+
 const ThemePage = () => {
-    return <div className="flex flex-col">theme here</div>;
+    const themeOptions: SegmentedControlItem[] = [
+        {
+            text: 'System',
+            iconWithNoClasses: <ComputerDesktopIcon />,
+            isActive: true,
+            onClick: () => null,
+        },
+        {
+            text: 'Light',
+            iconWithNoClasses: <SunIcon />,
+            isActive: false,
+            onClick: () => null,
+        },
+        {
+            text: 'Dark',
+            iconWithNoClasses: <MoonIcon />,
+            isActive: false,
+            onClick: () => null,
+        },
+    ];
+
+    return (
+        <div className="flex flex-col">
+            <ContentBlock className="!py-6">
+                <Header>Theme</Header>
+                <BodyLarge isTextColorMedium>
+                    Choose if you&apos;d like to have the lights on or off by
+                    toggling your theme below.
+                </BodyLarge>
+            </ContentBlock>
+            <SegmentedControl items={themeOptions} />
+        </div>
+    );
 };
 
 export default ThemePage;
