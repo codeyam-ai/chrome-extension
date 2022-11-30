d699f5d412db86b3b2ab8ade3c6928762de8d7ff
Adam Wade <adamwade2384@gmail.com>
2022-11-30 12:55:24 -0800

    Fixed the alert transaction receipt link

--- src/ui/app/pages/home/receipt/index.tsx
+++ src/ui/app/pages/home/receipt/index.tsx
@@ -1,14 +1,17 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-import { useMemo } from 'react';
-import { Navigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useMemo } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { Content } from '_app/shared/bottom-menu-layout';
 import ReceiptCard from '_components/receipt-card';
-import { useAppSelector } from '_hooks';
-import NavBarWithBackAndTitle from '_src/ui/app/shared/navigation/nav-bar/NavBarWithBackAndTitle';
+import { useAppDispatch, useAppSelector } from '_hooks';
+import Loading from '_src/ui/app/components/loading';
 
-import type { TxResultState } from '_redux/slices/txresults';
+import {
+    getTransactionsByAddress,
+    TxResultState,
+} from '_redux/slices/txresults';
 
 import st from './ReceiptPage.module.scss';
 
@@ -16,12 +19,23 @@ import st from './ReceiptPage.module.scss';
 // use txDigest for the transaction result
 function ReceiptPage() {
     const [searchParams] = useSearchParams();
+    const dispatch = useAppDispatch();
+
+    const navigate = useNavigate();
+
+    const loading = useAppSelector(({ txresults }) => txresults.loading);
+
+    useEffect(() => {
+        const getTxByAddr = async () => {
+            await dispatch(getTransactionsByAddress()).unwrap();
+        };
+
+        getTxByAddr();
+    }, [dispatch]);
 
     // get tx results from url params
     const txDigest = searchParams.get('txdigest');
-
     const tranferType = searchParams.get('transfer');
-
     const txResults: TxResultState[] = useAppSelector(
         ({ txresults }) => txresults.latestTx
     );
@@ -32,14 +46,18 @@ function ReceiptPage() {
 
     const linkTo = tranferType ? '/nfts' : '/transactions';
 
-    if (!txDigest || (txResults && !txnItem)) {
-        return <Navigate to={linkTo} replace={true} />;
+    if (!loading && (!txDigest || (txResults && !txnItem))) {
+        navigate(linkTo);
     }
 
     return (
-        <div className={st.container}>
-            <Content>{txnItem && <ReceiptCard txDigest={txnItem} />}</Content>
-        </div>
+        <Loading loading={loading} big={true}>
+            <div className={st.container}>
+                <Content>
+                    {txnItem && <ReceiptCard txDigest={txnItem} />}
+                </Content>
+            </div>
+        </Loading>
     );
 }
 
