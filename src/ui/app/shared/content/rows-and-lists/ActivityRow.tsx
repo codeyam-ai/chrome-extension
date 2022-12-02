ba8a58513fec28c4bf9c5547458df6ca275270f7
Adam Wade <adamwade2384@gmail.com>
2022-12-01 16:39:48 -0800

    Update for text color prop

--- src/ui/app/shared/content/rows-and-lists/ActivityRow.tsx
+++ src/ui/app/shared/content/rows-and-lists/ActivityRow.tsx
@@ -33,20 +33,14 @@ export const ActivityRow = ({
                 <div>{icon}</div>
                 <span className="flex flex-col text-left">
                     <BodyLarge>{header}</BodyLarge>
-                    <Body className={'text-ethos-light-text-medium'}>
-                        {subheader}
-                    </Body>
+                    <Body isTextColorMedium>{subheader}</Body>
                 </span>
             </div>
             <span className="flex flex-row justify-between">
                 {txAmount && (
                     <div className={'text-right'}>
-                        <BodyLarge className="float-right font-semibold">
-                            {formattedAmount}
-                        </BodyLarge>
-                        <Body className={'text-ethos-light-text-medium'}>
-                            {amountSubtext}
-                        </Body>
+                        <BodyLarge isSemibold>{formattedAmount}</BodyLarge>
+                        <Body isTextColorMedium>{amountSubtext}</Body>
                     </div>
                 )}
             </span>
