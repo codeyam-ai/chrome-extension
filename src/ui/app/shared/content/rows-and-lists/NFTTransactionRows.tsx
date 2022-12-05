51a1e8d9613801bd027c0236dd798819d8e70c18
Jared Cosulich <jared.cosulich@gmail.com>
2022-12-05 17:34:07 -0500

    commenting out fake data

--- src/ui/app/shared/content/rows-and-lists/NFTTransactionRows.tsx
+++ src/ui/app/shared/content/rows-and-lists/NFTTransactionRows.tsx
@@ -7,6 +7,60 @@ interface NftTransactionRowsProps {
 }
 
 const NftTransactionRows = ({ transactions }: NftTransactionRowsProps) => {
+    /*
+    
+    TODO: Determine where the event data comes from for NFT transactions. Are the
+    events already in the wallet or do the events need to be added.
+
+    -> Get transactions prop
+
+    */
+
+    // const tempData = [
+    //     {
+    //         txId: 'ID',
+    //         status: '',
+    //         txType: 'Sold',
+    //         toAddr: '0000000000000000000000',
+    //         fromAddr: '0000000000000000000000',
+    //         txAmount: 500000,
+    //         txGas: 10000,
+    //         coinType: 'SUI',
+    //         vendor: 'Clutchy',
+    //         date: 'Wed, Sept. 14',
+    //         kind: '',
+    //         from: '',
+    //     },
+    //     {
+    //         txId: 'ID',
+    //         status: '',
+    //         txType: 'Listed',
+    //         toAddr: '0000000000000000000000',
+    //         fromAddr: '0000000000000000000000',
+    //         txAmount: 500000,
+    //         txGas: 10000,
+    //         coinType: 'SUI',
+    //         vendor: 'Clutchy',
+    //         date: 'Wed, Sept. 14',
+    //         kind: '',
+    //         from: '',
+    //     },
+    //     {
+    //         txId: 'ID',
+    //         status: '',
+    //         txType: 'Sent',
+    //         toAddr: '0000000000000000000000',
+    //         fromAddr: '0000000000000000000000',
+    //         txAmount: 500000,
+    //         txGas: 10000,
+    //         coinType: 'SUI',
+    //         vendor: 'Clutchy',
+    //         date: 'Wed, Sept. 14',
+    //         kind: '',
+    //         from: '',
+    //     },
+    // ];
+
     return (
         <div className="pb-6 dark:divide-ethos-dark-text-stroke">
             {(transactions || []).map((txn, idx) => {
