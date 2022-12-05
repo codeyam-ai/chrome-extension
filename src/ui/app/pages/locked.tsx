f9bda9bd8cbb6f808d121458483deda776fe9b11
Jared Cosulich <jared.cosulich@gmail.com>
2022-12-05 07:58:12 -0500

    removing unneccessary import

--- src/ui/app/pages/locked.tsx
+++ src/ui/app/pages/locked.tsx
@@ -4,7 +4,6 @@
 import { useCallback, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-import useAppSelector from '../hooks/useAppSelector';
 import { AppState } from '../hooks/useInitializedGuard';
 import {
     loadAccountInformationFromStorage,
