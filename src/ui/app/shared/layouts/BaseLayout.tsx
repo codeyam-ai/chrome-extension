a649e8bf84837e22e123f8cb3c1b2807231a5bd7
Adam Wade <adamwade2384@gmail.com>
2022-12-05 10:32:13 -0800

    Update animation and theme for alerts

--- src/ui/app/shared/layouts/BaseLayout.tsx
+++ src/ui/app/shared/layouts/BaseLayout.tsx
@@ -1,14 +1,41 @@
+import { ToastContainer, cssTransition } from 'react-toastify';
+
 import type React from 'react';
+
+import 'animate.css/animate.min.css';
 
 export interface BaseLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
 }
 
+const Fade = cssTransition({
+    enter: 'animate__animated animate__fadeIn',
+    exit: 'animate__animated animate__fadeOut',
+});
+
 const BaseLayout = ({ className, children }: BaseLayoutProps) => {
+    const isDarkTheme = document
+        .getElementsByTagName('html')[0]
+        .classList.contains('dark');
+
     const defaultClasses =
         'mx-auto w-[360px] min-h-[600px] shadow-ethos-box-shadow sm:rounded-[20px] text-center bg-ethos-light-background-default dark:bg-ethos-dark-background-default';
     return (
-        <div className={`${className || ''} ${defaultClasses}`}>{children}</div>
+        <div className={`${className || ''} ${defaultClasses}`}>
+            <ToastContainer
+                position="top-center"
+                transition={Fade}
+                autoClose={2500}
+                hideProgressBar
+                newestOnTop={true}
+                closeOnClick={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme={isDarkTheme ? 'light' : 'dark'}
+            />
+            {children}
+        </div>
     );
 };
 
