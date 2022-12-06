4381161051529a75d955dd721a49e12cf6d56bd2
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-12-06 16:33:24 -0600

    Nav for supbages of settings menu

--- src/ui/app/components/settings-menu/SettingsRouterPage.tsx
+++ src/ui/app/components/settings-menu/SettingsRouterPage.tsx
@@ -58,11 +58,6 @@ function SettingsRouterPage() {
     }
     return (
         <div className="absolute flex flex-col h-[536px] w-full overflow-y-auto z-10 drop-shadow-ethos-box-shadow sm:rounded-[20px] bg-ethos-light-background-default dark:bg-ethos-dark-background-default">
-            {!settingsIsOpenOnSubPage ? (
-                <></>
-            ) : (
-                <NavBarWithBackAndWalletPicker backUrl={settingsHomeUrl} />
-            )}
             <div className="overflow-scroll no-scrollbar">
                 <Routes location={settingsUrl || ''}>
                     <Route path="/" element={<SettingsHomePage />} />
--- src/ui/app/shared/navigation/nav-bar/NavBar.tsx
+++ src/ui/app/shared/navigation/nav-bar/NavBar.tsx
@@ -1,4 +1,8 @@
-import { Cog6ToothIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import {
+    ArrowLeftIcon,
+    Cog6ToothIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/solid';
 import { Link, useNavigate } from 'react-router-dom';
 
 import WalletProfile from '../../content/rows-and-lists/WalletProfile';
@@ -6,24 +10,45 @@ import {
     useSettingsIsOpen,
     useNextSettingsUrl,
     useWalletPickerIsOpen,
+    useSettingsIsOpenOnSubPage,
 } from '_src/ui/app/components/settings-menu/hooks';
 import Header from '../../typography/Header';
 import { useCallback } from 'react';
+import NavBarWithBackAndWalletPicker from './NavBarWithBackAndWalletPicker';
+import BodyLarge from '../../typography/BodyLarge';
 
 const SettingsNavBar = () => {
     const navigate = useNavigate();
+    const settingsIsOpenOnSubPage = useSettingsIsOpenOnSubPage();
 
     const goBack = useCallback(() => {
         navigate(-1);
     }, [navigate]);
 
     return (
-        <div className="flex justify-between px-6 py-4 text-left border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke">
-            <Header>Settings</Header>
-            <button onClick={goBack}>
-                <XMarkIcon className="h-5 w-5 text-ethos-light-text-medium dark:text-ethos-dark-text-medium" />
-            </button>
-        </div>
+        <>
+            {!settingsIsOpenOnSubPage ? (
+                <div className="flex justify-between px-6 py-4 text-left border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke">
+                    <Header>Settings</Header>
+                    <button onClick={goBack}>
+                        <XMarkIcon className="h-5 w-5 text-ethos-light-text-medium dark:text-ethos-dark-text-medium" />
+                    </button>
+                </div>
+            ) : (
+                <div className="flex flex-row items-center justify-between px-6 py-4 border-b border-b-ethos-light-text-stroke dark:border-b-ethos-dark-text-stroke">
+                    <div className="flex flex-row gap-4 items-center">
+                        <button
+                            onClick={goBack}
+                            className="inline-flex flex-row gap-2 items-center text-ethos-light-text-medium dark:text-ethos-dark-text-medium"
+                        >
+                            <ArrowLeftIcon className="h-5 w-5" />
+                            <BodyLarge>Back</BodyLarge>
+                        </button>
+                    </div>
+                    <WalletProfile />
+                </div>
+            )}
+        </>
     );
 };
 
