b3a83db49eac88afb53125a925f95e3695c2fae9
Adam Wade <adamwade2384@gmail.com>
2022-12-09 11:35:51 -0800

    Add header to wallet list

--- src/ui/app/shared/wallet-list/WalletList.tsx
+++ src/ui/app/shared/wallet-list/WalletList.tsx
@@ -1,14 +1,17 @@
 import { type AccountInfo } from '../../KeypairVault';
+import BodyLarge from '../typography/BodyLarge';
 import WalletButton from './WalletButton';
 
-interface WalletListProps {
+export type WalletListProps = {
+    header?: string;
     hasTopPadding?: boolean;
     wallets: AccountInfo[];
     activeAccountIndex: number;
     isWalletEditing: boolean;
-}
+};
 
 const WalletList = ({
+    header,
     hasTopPadding,
     wallets,
     activeAccountIndex,
@@ -20,6 +23,13 @@ const WalletList = ({
                 isWalletEditing ? 'max-h-[482px]' : 'max-h-[432px]'
             } px-3 pb-4 flex flex-col gap-1 overflow-scroll no-scrollbar`}
         >
+            <BodyLarge
+                isSemibold
+                isTextColorMedium
+                className={'text-left pl-4'}
+            >
+                {header}
+            </BodyLarge>
             {wallets.map((wallet, key) => {
                 const isActive = (wallet.index || 0) === activeAccountIndex;
                 return (
