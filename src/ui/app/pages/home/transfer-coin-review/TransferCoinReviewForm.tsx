dbdfc4bc4e19c7e9a6a87b655d63aafe6c30e6de
Adam Wade <adamwade2384@gmail.com>
2022-12-12 14:01:26 -0800

    Add redirect for the review page if fields are empty

--- src/ui/app/pages/home/transfer-coin-review/TransferCoinReviewForm.tsx
+++ src/ui/app/pages/home/transfer-coin-review/TransferCoinReviewForm.tsx
@@ -1,41 +1,30 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import { ErrorMessage, Field, Form, useFormikContext } from 'formik';
-import { useEffect, useRef, memo, useCallback, MouseEventHandler } from 'react';
+import { Form, useFormikContext } from 'formik';
+import { useEffect, useRef, memo } from 'react';
 
 import { getTheme } from '_src/ui/app/helpers/getTheme';
 
-import AddressInput from '_components/address-input';
 import LoadingIndicator from '_components/loading/LoadingIndicator';
-import NumberInput from '_components/number-input';
-import { SuiIcons } from '_font-icons/output/sui-icons';
-import { GAS_SYMBOL, GAS_TYPE_ARG } from '_redux/slices/sui-objects/Coin';
-import Icon from '_src/ui/app/components/icon';
-import NFTDisplayCard from '_src/ui/app/components/nft-display';
-import { useAppSelector, useFormatCoin } from '_src/ui/app/hooks';
+import { useAppSelector } from '_src/ui/app/hooks';
 import Button from '_src/ui/app/shared/buttons/Button';
 import KeyValueList from '_src/ui/app/shared/content/rows-and-lists/KeyValueList';
-import Alert from '_src/ui/app/shared/feedback/Alert';
 import { AssetCard } from '_src/ui/app/shared/nfts/AssetCard';
-import SuiIcon from '_src/ui/app/shared/svg/SuiIcon';
 import Body from '_src/ui/app/shared/typography/Body';
 import Header from '_src/ui/app/shared/typography/Header';
 import Subheader from '_src/ui/app/shared/typography/Subheader';
 
 import type { FormValues } from '.';
 import truncateMiddle from '_src/ui/app/helpers/truncate-middle';
+import { Navigate } from 'react-router-dom';
 
 export type TransferCoinFormProps = {
     submitError: string | null;
     onClearSubmitError: () => void;
 };
 
-function TransferCoinForm({
-    submitError,
-    onClearSubmitError,
-}: TransferCoinFormProps) {
+function TransferCoinForm({ onClearSubmitError }: TransferCoinFormProps) {
     const {
         isSubmitting,
         values: { amount, to },
@@ -54,6 +43,10 @@ function TransferCoinForm({
         style: 'currency',
         currency: 'USD',
     }).format(parseFloat(amount) * 100);
+
+    if (amount === '' || to === '') {
+        return <Navigate to={'/tokens'} />;
+    }
 
     return (
         <Form autoComplete="off" noValidate={true}>
