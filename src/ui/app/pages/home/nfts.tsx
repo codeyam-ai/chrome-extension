0f8bea3dd2af071b12abcee943d0e5af341aa78d
Adam Wade <adamwade2384@gmail.com>
2022-12-16 13:06:18 -0800

    Add loader to nft list route

--- src/ui/app/pages/home/nfts.tsx
+++ src/ui/app/pages/home/nfts.tsx
@@ -10,26 +10,29 @@ import { NFT_EXPERIMENT_LINK } from '_src/shared/constants';
 import NftGrid from '_src/ui/app/shared/content/rows-and-lists/NftGrid';
 import TextPageTitle from '_src/ui/app/shared/headers/page-headers/TextPageTitle';
 import EmptyPageState from '_src/ui/app/shared/layouts/EmptyPageState';
+import Loading from '../../components/loading';
 
 function NftsPage() {
     const nfts = useAppSelector(accountNftsSelector) || [];
 
     return (
         <div>
-            {nfts.length === 0 ? (
-                <EmptyPageState
-                    iconWithNoClasses={<Icon displayIcon={<PhotoIcon />} />}
-                    title="No NFTs here yet"
-                    subtitle="This is where your created or purchased NFTs will appear..."
-                    linkText="Explore NFTs on Ethos"
-                    linkUrl={NFT_EXPERIMENT_LINK}
-                />
-            ) : (
-                <>
-                    <TextPageTitle title="NFTs" count={nfts.length} />
-                    <NftGrid nfts={nfts} />
-                </>
-            )}
+            <Loading loading={nfts.length === 0} big={true}>
+                {nfts.length === 0 ? (
+                    <EmptyPageState
+                        iconWithNoClasses={<Icon displayIcon={<PhotoIcon />} />}
+                        title="No NFTs here yet"
+                        subtitle="This is where your created or purchased NFTs will appear..."
+                        linkText="Explore NFTs on Ethos"
+                        linkUrl={NFT_EXPERIMENT_LINK}
+                    />
+                ) : (
+                    <>
+                        <TextPageTitle title="NFTs" count={nfts.length} />
+                        <NftGrid nfts={nfts} />
+                    </>
+                )}
+            </Loading>
         </div>
     );
 }
