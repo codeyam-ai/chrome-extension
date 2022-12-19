ed04f11f85250d51b14d9314d3e0803da0aff048
Jared Cosulich <jared.cosulich@gmail.com>
2022-12-19 11:07:54 -0500

    using coin metadata

--- src/ui/app/helpers/truncate-string.ts
+++ src/ui/app/helpers/truncate-string.ts
@@ -1,6 +1,6 @@
 export default function truncateString(str: string, num: number) {
     if (str.length > num) {
-        return str.slice(0, num) + '...';
+        return str.slice(0, num - 2) + '...';
     } else {
         return str;
     }
