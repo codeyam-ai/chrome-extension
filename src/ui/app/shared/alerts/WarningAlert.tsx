5f0cd62547cdae1cdfe15e86a89578f0000aa78b
Adam Wade <adamwade2384@gmail.com>
2023-01-04 21:14:45 -0800

    Add warning alert

---
+++ src/ui/app/shared/alerts/WarningAlert.tsx
@@ -0,0 +1,14 @@
+import { ExclaimationTriangleIcon } from '@heroicons/react/24/solid';
+
+type WarningAlertProps = {
+    text: string;
+};
+
+export const WarningAlert = ({ text }: WarningAlertProps) => (
+    <div className={'flex flex-row justify-between'}>
+        <div className={'flex flex-row gap-2 items-center'}>
+            <ExclaimationTriangleIcon width={20} height={20} color={'yellow'} />
+            <div className={'font-medium'}>{text}</div>
+        </div>
+    </div>
+);
