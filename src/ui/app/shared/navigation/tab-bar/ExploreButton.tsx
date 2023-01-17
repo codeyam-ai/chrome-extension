162856127731f8ffce66ccc7a02419a08837abf3
Jared Cosulich <jared.cosulich@gmail.com>
2023-01-17 14:28:10 -0500

    fix explore button signing in

--- src/ui/app/shared/navigation/tab-bar/ExploreButton.tsx
+++ src/ui/app/shared/navigation/tab-bar/ExploreButton.tsx
@@ -1,9 +1,15 @@
 import exploreButtonSrc from '_assets/images/explore-button.png';
 import { DASHBOARD_LINK } from '_src/shared/constants';
+import { useExplorerPermission } from '_src/ui/app/hooks';
 
 const ExploreButton = () => {
+    const setExplorerPermission = useExplorerPermission();
+
     return (
-        <div className="flex flex-row justify-center items-center">
+        <div
+            className="flex flex-row justify-center items-center"
+            onMouseOver={setExplorerPermission}
+        >
             <a
                 href={DASHBOARD_LINK}
                 target="_blank"
