812096aff400b0054b457b0662b331f59b4130c1
Jared Cosulich <jared.cosulich@gmail.com>
2023-01-21 07:58:24 -0500

    working on vote approval screen

--- src/ui/app/pages/dapp-tx-approval/summaries/CapyVote.tsx
+++ src/ui/app/pages/dapp-tx-approval/summaries/CapyVote.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
+import { useMemo } from 'react';
 
-import { api } from '_src/ui/app/redux/store/thunk-extras';
 import Body from '_src/ui/app/shared/typography/Body';
 
 import type { SummaryGeneratorArgs } from './standard';
@@ -15,42 +14,49 @@ const CapyVote = ({
     totalSymbol,
     totalDollars,
 }: SummaryGeneratorArgs) => {
-    useEffect(() => {
-        if (!address) return;
+    const { uuid, name } = useMemo(() => {
+        if (!address) return { uuid: '', name: '' };
 
-        const getCapy = async () => {
-            if (
-                typeof txInfo === 'object' &&
-                'data' in txInfo &&
-                'packageObjectId' in txInfo.data
-            ) {
-                const response = await api.instance.fullNode.devInspectMoveCall(
-                    address,
-                    {
-                        packageObjectId: txInfo.data.packageObjectId,
-                        module: 'capy_election',
-                        function: 'get_candidate',
-                        typeArguments: [],
-                        arguments: txInfo.data.arguments,
-                    }
-                );
+        if (
+            typeof txInfo === 'object' &&
+            'data' in txInfo &&
+            'packageObjectId' in txInfo.data &&
+            typeof txInfo.data.arguments[0] === 'string'
+        ) {
+            const [uuid, name] = txInfo.data.arguments[0]
+                .replace(/\+/g, ' ')
+                .split('--');
+            return { uuid, name };
+        }
 
-                console.log('response', response);
-            }
-        };
-
-        getCapy();
+        return { uuid: '', name: '' };
     }, [txInfo, address]);
 
     return (
-        <div className="bg-[#F3F9FF] p-6 py-6 flex-col gap-6 rounded-2xl text-black">
-            <div className="text-2xl">Vote for</div>
+        <div className="bg-[#F3F9FF] p-6 flex flex-col gap-6 rounded-2xl text-black">
+            <div>
+                <div className="text-2xl font-semibold">Vote for</div>
+                <div className="text-lg">{name}</div>
+            </div>
+            <div className="w-full aspect-square">
+                {uuid && uuid.length > 0 && (
+                    <img
+                        src={`https://ai-capy.s3.amazonaws.com/${uuid}`}
+                        alt="Capy"
+                        className="w-full aspect-square rounded-xl"
+                    />
+                )}
+            </div>
 
+            <Body isSemibold>Cost</Body>
             <div className="flex flex-col gap-3">
-                <Body isSemibold>Cost</Body>
-                <div className="flex justify-between items-center border-b">
+                <div className="flex justify-between items-center border-b pb-3 text-sm border-black">
                     <div>Gas Fee</div>
-                    <div>{formattedGas}</div>
+                    <div>{formattedGas} SUI</div>
+                </div>
+                <div className="flex justify-between items-center">
+                    <Body isSemibold>Total</Body>
+                    <Body isSemibold>{formattedGas} SUI</Body>
                 </div>
             </div>
         </div>
