14c246b6899dad3d3fff80b02147762ca2bc6429
Jared Cosulich <jared.cosulich@gmail.com>
2023-01-22 09:06:38 -0500

    integrating bag NFTs

--- src/ui/app/hooks/useNFTBasicData.ts
+++ src/ui/app/hooks/useNFTBasicData.ts
@@ -2,24 +2,47 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { getObjectId, getObjectFields } from '@mysten/sui.js';
+import { useEffect, useState } from 'react';
 
 import useFileExtentionType from './useFileExtentionType';
 import useMediaUrl from './useMediaUrl';
+import { NFT } from '_redux/slices/sui-objects/NFT';
+import { api } from '_redux/store/thunk-extras';
 
 import type { SuiObject } from '@mysten/sui.js';
 
 export default function useNFTBasicData(nftObj: SuiObject) {
     const nftObjectID = getObjectId(nftObj.reference);
-    const filePath = useMediaUrl(nftObj.data);
-    const nftFields =
+
+    const defaultFilePath = useMediaUrl(nftObj.data);
+    const [filePath, setFilePath] = useState(defaultFilePath);
+    const fileExtentionType = useFileExtentionType(filePath || '');
+
+    const defaultNftFields =
         nftObj.data.dataType === 'moveObject'
             ? getObjectFields(nftObj.data)
             : null;
-    const fileExtentionType = useFileExtentionType(filePath || '');
+    const [nftFields, setNftFields] = useState(defaultNftFields);
+
+    useEffect(() => {
+        if (!NFT.isBagNFT(nftObj)) return;
+
+        const getBagNft = async () => {
+            const provider = api.instance.fullNode;
+            const bagNFT = await NFT.parseBagNFT(provider, nftObj);
+            if ('url' in bagNFT && bagNFT.url) {
+                setNftFields(bagNFT);
+                setFilePath(bagNFT.url);
+            }
+        };
+
+        getBagNft();
+    }, [nftObj]);
+
     return {
         nftObjectID,
         filePath,
-        nftFields,
         fileExtentionType,
+        nftFields,
     };
 }
