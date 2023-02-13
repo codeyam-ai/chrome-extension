93ac184f0ba84c8017daf580560d9f757366723c
Jared Cosulich <jared.cosulich@gmail.com>
2023-02-13 14:23:51 -0800

    remove thrown error

--- src/background/Transactions.ts
+++ src/background/Transactions.ts
@@ -380,9 +380,6 @@ class Transactions {
 
         const { sui_Env } = await Browser.storage.local.get('sui_Env');
         const endpoint = api.getEndPoints(sui_Env).fullNode;
-        if (endpoint || !endpoint) {
-            throw new Error(`ENDPOINT: ${endpoint}`);
-        }
         const response = await fetch(endpoint, callData);
         const json = await response.json();
 
