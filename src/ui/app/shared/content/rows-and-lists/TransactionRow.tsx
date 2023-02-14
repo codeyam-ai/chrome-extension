f09715098bcc5039d384e3dc857de53ba38aa377
Adam Wade <adamwade2384@gmail.com>
2023-02-14 15:15:03 -0800

    Add in tx row data map and formatting

--- src/ui/app/shared/content/rows-and-lists/TransactionRow.tsx
+++ src/ui/app/shared/content/rows-and-lists/TransactionRow.tsx
@@ -24,16 +24,22 @@ import type { TxResultState } from '_src/ui/app/redux/slices/txresults';
 import { getHumanReadable } from '_src/ui/app/helpers/transactions';
 import type { FormattedCoin } from '_src/ui/app/pages/home/transactions/FormattedCoin';
 import type { txnType } from '_src/ui/app/pages/home/transactions';
+import { toNamespacedPath } from 'path';
 
 interface TransactionRowProps {
     txn: txnType;
+}
+
+interface RowDataTypes extends SharedTypes {
+    header?: string;
+    typeIcon: JSX.Element;
+    icon: JSX.Element;
 }
 
 const TRUNCATE_MAX_LENGTH = 8;
 const TRUNCATE_PREFIX_LENGTH = 4;
 
 interface SharedTypes {
-    txFailed: boolean;
     hasAmount: boolean;
     amount?: number;
     coinType: string;
@@ -56,44 +62,226 @@ const TransactionRow = ({ txn }: TransactionRowProps) => {
         verb,
     } = getHumanReadable(txn);
 
-    console.log('txn: ', txn);
-
     const drilldownLink = `/transactions/receipt?${new URLSearchParams({
         txdigest: txn?.txId,
         symbol: 'SUI',
         isFunc: txType === 'func' ? 'yes' : 'no',
     }).toString()}`;
 
-    console.log(
-        'CHECK: ',
-        txType,
-        txAction,
-        nftImageUri,
-        otherAddress,
-        otherAddressStr,
-        preposition,
-        subject,
-        timeDisplay,
-        verb
+    const shared: SharedTypes = {
+        hasAmount: (txn.amount && txn.amount > 0) || false,
+        amount: txn?.amount || undefined,
+        coinType: txn?.coinType || '',
+        type: txAction || '',
+        txDirText:
+            txAction === 'send'
+                ? `To ${otherAddressStr}`
+                : `From ${otherAddressStr}` || '',
+        link: drilldownLink,
+        date: timeDisplay,
+    };
+
+    const IconContainer = ({ children }: { children: JSX.Element }) => (
+        <div
+            className={
+                'flex w-[40px] h-[40px] justify-center items-center bg-[#3D5FF2] rounded-full'
+            }
+        >
+            {children}
+        </div>
     );
+
+    const FunctionIcon = () => (
+        <IconContainer>
+            <CodeBracketSquareIcon width={20} height={20} color={'white'} />
+        </IconContainer>
+    );
+
+    const CurrencyIcon = () => (
+        <>
+            {txn.formatted.coinSymbol.toLowerCase() === 'sui' ? (
+                <IconContainer>
+                    <SuiIcon width={14} height={20} color={'white'} />
+                </IconContainer>
+            ) : txn.formatted.coinIcon ? (
+                <img
+                    src={txn.formatted.coinIcon}
+                    alt={`Icon for ${txn.formatted.coinSymbol}`}
+                    className="w-10 h-10"
+                />
+            ) : (
+                <UnknownToken width={40} height={40} />
+            )}
+        </>
+    );
+
+    const NftImg = ({ src, alt }: { src: string; alt: string }) => (
+        <img className={'w-[40px] h-[40px] rounded-lg'} src={src} alt={alt} />
+    );
+
+    const iconProps = { color: '#74777C', width: 18, height: 18 };
+
+    console.log('txn', txn.formatted);
+
+    const dataMap: {
+        nft: {
+            [key: string]: RowDataTypes;
+        };
+        sui: {
+            [key: string]: RowDataTypes;
+        };
+        coin: {
+            [key: string]: RowDataTypes;
+        };
+        function: {
+            [key: string]: RowDataTypes;
+        };
+    } = {
+        nft: {
+            Send: {
+                ...shared,
+                typeIcon: <ArrowUpIcon {...iconProps} />,
+                icon: (
+                    <NftImg
+                        src={nftImageUri || ''}
+                        alt={txn.description || ''}
+                    />
+                ),
+                header: txn?.name,
+            },
+            Receive: {
+                ...shared,
+                typeIcon: <ArrowDownIcon {...iconProps} />,
+                icon: (
+                    <NftImg
+                        src={nftImageUri || ''}
+                        alt={txn.description || ''}
+                    />
+                ),
+                header: txn?.name,
+            },
+            Mint: {
+                ...shared,
+                typeIcon: <SparklesIcon {...iconProps} />,
+                icon: (
+                    <NftImg
+                        src={nftImageUri || ''}
+                        alt={txn.description || ''}
+                    />
+                ),
+                header: txn?.name,
+            },
+            register: {
+                ...shared,
+                typeIcon: <SparklesIcon {...iconProps} />,
+                icon: nftImageUri ? (
+                    <NftImg
+                        src={ipfs(nftImageUri)}
+                        alt={txn.description || ''}
+                    />
+                ) : (
+                    <></>
+                ),
+                header: txn.formatted.coinSymbol || 'Sui Action',
+            },
+        },
+        sui: {
+            Send: {
+                ...shared,
+                typeIcon: <ArrowUpIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+            Receive: {
+                ...shared,
+                typeIcon: <ArrowDownIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+            Mint: {
+                ...shared,
+                typeIcon: <SparklesIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+        },
+        coin: {
+            Send: {
+                ...shared,
+                typeIcon: <ArrowUpIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+            Receive: {
+                ...shared,
+                typeIcon: <ArrowDownIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+            Mint: {
+                ...shared,
+                typeIcon: <SparklesIcon {...iconProps} />,
+                icon: <CurrencyIcon />,
+                header: txn.formatted.coinSymbol,
+            },
+        },
+        function: {
+            modify: {
+                ...shared,
+                typeIcon: <PencilSquareIcon {...iconProps} />,
+                icon: <FunctionIcon />,
+                header: 'Sui Action',
+            },
+            burn: {
+                ...shared,
+                typeIcon: <FireIcon {...iconProps} />,
+                icon: <FunctionIcon />,
+                header: 'Sui Action',
+            },
+            transfer: {
+                ...shared,
+                typeIcon: <ArrowsRightLeftIcon {...iconProps} />,
+                icon: <FunctionIcon />,
+                header: 'Sui Action',
+            },
+            default: {
+                ...shared,
+                typeIcon: <SuiIcon {...iconProps} />,
+                icon: <FunctionIcon />,
+                header: 'Sui Action',
+            },
+        },
+    };
+
+    let rowData;
+
+    if (!txType) return <></>;
+
+    if (txType === 'nft') {
+        rowData = dataMap.nft[txType];
+    } else if (txn.formatted.coinSymbol.toLowerCase() === 'sui') {
+        rowData = dataMap.sui[txType];
+    } else if (txn.type === 'call') {
+        rowData = dataMap.function[txType || 'default'];
+    } else {
+        rowData = dataMap.coin[txType];
+    }
+
+    if (!rowData || !txn.formatted) return <></>;
 
     return (
         <ActivityRow
-            failed={false}
-            typeIcon={<CodeBracketSquareIcon width={20} height={20} />}
-            type={txType}
-            date={timeDisplay}
-            icon={<CodeBracketSquareIcon width={20} height={20} />}
-            link={drilldownLink}
-            header={txn.formatted.coinSymbol}
-            subheader={txAction === 'send' ? 'Sent' : 'Received'}
-            formattedAmount={
-                txn.formatted.formattedBalance
-                    ? txn.formatted.formattedBalance
-                    : '0'
-            }
-            symbol={txn.formatted.coinSymbol ? txn.formatted.coinSymbol : 'SUI'}
-            dollars={txn.formatted.dollars ? txn.formatted.dollars : '0'}
+            failed={txn.status === 'failure'}
+            typeIcon={rowData.typeIcon}
+            type={rowData.type}
+            date={rowData.date}
+            icon={rowData.icon}
+            link={rowData.link}
+            header={rowData.header}
+            subheader={rowData.txDirText}
+            formattedAmount={txn.formatted.formattedBalance}
+            symbol={txn.formatted.coinSymbol}
+            dollars={txn.formatted.dollars}
         />
     );
 };
