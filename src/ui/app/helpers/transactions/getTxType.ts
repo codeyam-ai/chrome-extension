2924bbd7f754635e6d73c5777b420377f21b2a23
Adam Wade <adamwade2384@gmail.com>
2023-02-15 21:53:27 -0800

    Add pay all sui to sui type

--- src/ui/app/helpers/transactions/getTxType.ts
+++ src/ui/app/helpers/transactions/getTxType.ts
@@ -5,7 +5,7 @@ export type TxType = 'nft' | 'sui' | 'func' | 'coin';
 const getTxType = (txn: TxResultState): TxType => {
     if (txn?.callFunctionName === 'transfer' || txn?.objectId?.length) {
         return 'nft';
-    } else if (txn?.kind === 'PaySui') {
+    } else if (txn?.kind === 'PaySui' || txn?.kind === 'PayAllSui') {
         return 'sui';
     } else if (txn?.callFunctionName && txn?.callFunctionName !== 'mint') {
         return 'func';
