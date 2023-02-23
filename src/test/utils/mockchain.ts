d6e5b1795f2d5f524c4c4ed2db8dd9d658157ccc
Mike Grafton <2266+mag@users.noreply.github.com>
2023-02-22 16:58:52 -0800

    Extract a helpful method

--- src/test/utils/mockchain.ts
+++ src/test/utils/mockchain.ts
@@ -1,31 +1,47 @@
 import { readFileSync } from 'fs';
+import _ from 'lodash';
+import { v4 as uuidV4 } from 'uuid';
+
 import nock from 'nock';
 import { join } from 'path';
 import { renderTemplate } from './json-templates';
 
+export const mockBlockchainCall = function (
+    request: { method: string; params?: any },
+    result: any,
+    persist?: boolean
+) {
+    let theNock = nock('http://devNet-fullnode.example.com');
+    if (persist) {
+        theNock = theNock.persist();
+    }
+    theNock.post('/', _.matches(request)).reply(200, {
+        jsonrpc: '2.0',
+        result: result,
+        id: uuidV4.toString(),
+    });
+};
 
 export const mockCommonCalls = function () {
-    nock('http://devNet-fullnode.example.com')
-        .persist()
-        .post('/', /rpc.discover/)
-        .reply(200, {
-            jsonrpc: '2.0',
-            result: { info: { version: '0.20.1' } },
-            id: 'fbf9bf0c-a3c9-460a-a999-b7e87096dd1c',
-        })
-        .post('/', /sui_getCoinMetadata/)
-        .reply(200, {
-            jsonrpc: '2.0',
-            result: {
-                decimals: 9,
-                name: 'Sui',
-                symbol: 'SUI',
-                description: '',
-                iconUrl: null,
-                id: null,
-            },
-            id: 'fbf9bf0c-a3c9-460a-a999-b7e87096dd1c',
-        });
+    mockBlockchainCall(
+        { method: 'rpc.discover' },
+        { info: { version: '0.20.1' } },
+        true
+    );
+
+    const coinMetadataResponse = {
+        decimals: 9,
+        name: 'Sui',
+        symbol: 'SUI',
+        description: '',
+        iconUrl: null,
+        id: null,
+    };
+    mockBlockchainCall(
+        { method: 'sui_getCoinMetadata' },
+        coinMetadataResponse,
+        true
+    );
 };
 
 export const mockSuiObjects = function (
@@ -50,13 +66,13 @@ export const mockSuiObjects = function (
         renderedObjects.push(renderedNftResult);
     }
 
-    const renderedResponses = renderedObjects.map(
-        (value) => {
-            return {jsonrpc: "2.0", id: "fbf9bf0c-a3c9-460a-a999-b7e87096dd1c", result: value}
-        }
-    );
-    // const finalRenderedTemplate = `[${renderedResponses.join(',')}]`;
-
+    const renderedResponses = renderedObjects.map((value) => {
+        return {
+            jsonrpc: '2.0',
+            id: 'fbf9bf0c-a3c9-460a-a999-b7e87096dd1c',
+            result: value,
+        };
+    });
     // TODO: make this work for both wallets that are in play for the user (see fake-local-storage).
 
     // TODO: clean this up.  in the case where renderedResponses is an empty array (i.e. fresh wallet with nothing in
