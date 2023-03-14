1df173c795b72abde59676f32a55dca5727df17b
Adam Wade <adamwade2384@gmail.com>
2023-03-14 15:31:14 -0700

    refactor getObjTypeFromObjId

--- src/ui/app/helpers/transactions/getObjTypeFromObjId.ts
+++ src/ui/app/helpers/transactions/getObjTypeFromObjId.ts
@@ -19,11 +19,11 @@ const getObjTypeFromObjId = async (
         obj.details &&
         typeof obj.details === 'object' &&
         'data' in obj.details &&
-        'type' in obj.details.data
+        'type' in obj.details
     ) {
         // Returns something like this: 0x2::coin::Coin<0xe881966c6a8b405485d014087a32d712c499d81b::af::AF>
         // But all we need is 0xe881966c6a8b405485d014087a32d712c499d81b::af::AF
-        const rawObjType = obj.details.data.type;
+        const rawObjType = obj.details.type as string;
         return cleanRawObjType(rawObjType);
     }
     return;
