c033a5d492cf27adca7b0e535530717118d0f60e
Mike Grafton <2266+mag@users.noreply.github.com>
2023-03-14 15:14:19 -0700

    Comment out most of useNormalizedFunction

--- src/ui/app/pages/dapp-tx-approval/lib/useNormalizedFunction.ts
+++ src/ui/app/pages/dapp-tx-approval/lib/useNormalizedFunction.ts
@@ -3,40 +3,40 @@ import { useEffect, useState } from 'react';
 import { thunkExtras } from '_src/ui/app/redux/store/thunk-extras';
 
 import type { SuiMoveNormalizedFunction } from '@mysten/sui.js';
-import type { TransactionRequest } from '_src/shared/messaging/messages/payloads/transactions';
+import type { ApprovalRequest } from '_src/shared/messaging/messages/payloads/transactions';
 
-const useNormalizedFunction = (txRequest: TransactionRequest | null) => {
+const useNormalizedFunction = (txRequest: ApprovalRequest | null) => {
     const [normalizedFunction, setNormalizedFunction] = useState<
         SuiMoveNormalizedFunction | undefined
     >();
 
-    useEffect(() => {
-        const getNormalizedFunction = async () => {
-            if (!txRequest) return [];
-            if (txRequest.tx.type === 'move-call') return [];
-            if (typeof txRequest.tx.data === 'string') return [];
-            if (!('packageObjectId' in txRequest.tx.data.data)) return [];
-
-            const {
-                packageObjectId,
-                module,
-                function: func,
-            } = txRequest.tx.data.data;
-
-            const normalizedFunction =
-                await thunkExtras.api.instance.fullNode.getNormalizedMoveFunction(
-                    {
-                        package: packageObjectId,
-                        module: module,
-                        function: func,
-                    }
-                );
-            setNormalizedFunction(normalizedFunction);
-        };
-
-        getNormalizedFunction();
-    }, [txRequest]);
-
+    // useEffect(() => {
+    //     const getNormalizedFunction = async () => {
+    //         if (!txRequest) return [];
+    //         if (txRequest.tx.type === 'transaction') return [];
+    //         if (typeof txRequest.tx.data === 'string') return [];
+    //         if (!('packageObjectId' in txRequest.tx.data.data)) return [];
+    //
+    //         const {
+    //             packageObjectId,
+    //             module,
+    //             function: func,
+    //         } = txRequest.tx.data.data;
+    //
+    //         const normalizedFunction =
+    //             await thunkExtras.api.instance.fullNode.getNormalizedMoveFunction(
+    //                 {
+    //                     package: packageObjectId,
+    //                     module: module,
+    //                     function: func,
+    //                 }
+    //             );
+    //         setNormalizedFunction(normalizedFunction);
+    //     };
+    //
+    //     getNormalizedFunction();
+    // }, [txRequest]);
+    //
     return normalizedFunction;
 };
 
