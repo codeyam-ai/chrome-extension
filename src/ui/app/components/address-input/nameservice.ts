87c9b3ef7640f788259cdb32df8dcfa6d84c1846
Jared Cosulich <jared.cosulich@gmail.com>
2023-03-21 17:32:18 -0400

    commenting out nameservice temporarily

--- src/ui/app/components/address-input/nameservice.ts
+++ src/ui/app/components/address-input/nameservice.ts
@@ -34,107 +34,109 @@ const getNameserviceValues = async () => {
 };
 
 export const getSuiName = async (address: string, sender: string = SENDER) => {
-    const suiProvider = api.instance.fullNode;
-    try {
-        const { packageAddress, registryAddress } =
-            await getNameserviceValues();
+    return address;
+    // const suiProvider = api.instance.fullNode;
+    // // try {
+    //     const { packageAddress, registryAddress } =
+    //         await getNameserviceValues();
 
-        const registryTx = new Transaction();
-        registryTx.add(
-            Transaction.Commands.MoveCall({
-                target: `${packageAddress}::base_registry::get_record_by_key`,
-                arguments: [
-                    registryTx.object(registryAddress),
-                    registryTx.pure(`${trimAddress(address)}.addr.reverse`),
-                ],
-            })
-        );
-        const resolverBytes = get(
-            await suiProvider.devInspectTransaction({
-                sender: sender,
-                transaction: registryTx,
-            }),
-            DEV_INSPECT_RESULT_PATH_1
-        );
-        if (!resolverBytes) return address;
+    //     const registryTx = new Transaction();
+    //     registryTx.add(
+    //         Transaction.Commands.MoveCall({
+    //             target: `${packageAddress}::base_registry::get_record_by_key`,
+    //             arguments: [
+    //                 registryTx.object(registryAddress),
+    //                 registryTx.pure(`${trimAddress(address)}.addr.reverse`),
+    //             ],
+    //         })
+    //     );
+    //     const resolverBytes = get(
+    //         await suiProvider.devInspectTransaction({
+    //             sender: sender,
+    //             transaction: registryTx,
+    //         }),
+    //         DEV_INSPECT_RESULT_PATH_1
+    //     );
+    //     if (!resolverBytes) return address;
 
-        const resolver = toFullAddress(toHexString(resolverBytes));
-        const resolverTx = new Transaction();
-        resolverTx.add(
-            Transaction.Commands.MoveCall({
-                target: `${packageAddress}::resolver::name`,
-                arguments: [
-                    registryTx.object(resolver),
-                    registryTx.pure(address),
-                ],
-            })
-        );
-        const resolverResponse = await suiProvider.devInspectTransaction({
-            sender: sender,
-            transaction: resolverTx,
-        });
+    //     const resolver = toFullAddress(toHexString(resolverBytes));
+    //     const resolverTx = new Transaction();
+    //     resolverTx.add(
+    //         Transaction.Commands.MoveCall({
+    //             target: `${packageAddress}::resolver::name`,
+    //             arguments: [
+    //                 registryTx.object(resolver),
+    //                 registryTx.pure(address),
+    //             ],
+    //         })
+    //     );
+    //     const resolverResponse = await suiProvider.devInspectTransaction({
+    //         sender: sender,
+    //         transaction: resolverTx,
+    //     });
 
-        const nameByteArray = get(resolverResponse, DEV_INSPECT_RESULT_PATH_0);
-        if (!nameByteArray) return address;
+    //     const nameByteArray = get(resolverResponse, DEV_INSPECT_RESULT_PATH_0);
+    //     if (!nameByteArray) return address;
 
-        const name = toString(nameByteArray);
-        return name;
-    } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log('Error retreiving SuiNS Name', e);
-        return address;
-    }
+    //     const name = toString(nameByteArray);
+    //     return name;
+    // // } catch (e) {
+    // //     // eslint-disable-next-line no-console
+    // //     console.log('Error retreiving SuiNS Name', e);
+    // //     return address;
+    // // }
 };
 
 export const getSuiAddress = async (
     domain: string,
     sender: string = SENDER
 ) => {
-    const suiProvider = api.instance.fullNode;
+    return domain;
+    // const suiProvider = api.instance.fullNode;
 
-    const { packageAddress, registryAddress } = await getNameserviceValues();
+    // const { packageAddress, registryAddress } = await getNameserviceValues();
 
-    try {
-        const registryTx = new Transaction();
-        registryTx.add(
-            Transaction.Commands.MoveCall({
-                target: `${packageAddress}::base_registry::get_record_by_key`,
-                arguments: [
-                    registryTx.object(registryAddress),
-                    registryTx.pure(domain),
-                ],
-            })
-        );
-        const resolverResponse = await suiProvider.devInspectTransaction({
-            sender: sender,
-            transaction: registryTx,
-        });
-        const resolverBytes = get(resolverResponse, DEV_INSPECT_RESULT_PATH_1);
-        if (!resolverBytes) return domain;
+    // // try {
+    //     const registryTx = new Transaction();
+    //     registryTx.add(
+    //         Transaction.Commands.MoveCall({
+    //             target: `${packageAddress}::base_registry::get_record_by_key`,
+    //             arguments: [
+    //                 registryTx.object(registryAddress),
+    //                 registryTx.pure(domain),
+    //             ],
+    //         })
+    //     );
+    //     const resolverResponse = await suiProvider.devInspectTransaction({
+    //         sender: sender,
+    //         transaction: registryTx,
+    //     });
+    //     const resolverBytes = get(resolverResponse, DEV_INSPECT_RESULT_PATH_1);
+    //     if (!resolverBytes) return domain;
 
-        const resolver = toFullAddress(toHexString(resolverBytes));
-        const resolverTx = new Transaction();
-        resolverTx.add(
-            Transaction.Commands.MoveCall({
-                target: `${packageAddress}::resolver::addr`,
-                arguments: [
-                    registryTx.object(resolver),
-                    registryTx.pure(domain),
-                ],
-            })
-        );
-        const resolverResponse2 = await suiProvider.devInspectTransaction({
-            sender: sender,
-            transaction: resolverTx,
-        });
-        const addr = get(resolverResponse2, DEV_INSPECT_RESULT_PATH_0);
+    //     const resolver = toFullAddress(toHexString(resolverBytes));
+    //     const resolverTx = new Transaction();
+    //     resolverTx.add(
+    //         Transaction.Commands.MoveCall({
+    //             target: `${packageAddress}::resolver::addr`,
+    //             arguments: [
+    //                 registryTx.object(resolver),
+    //                 registryTx.pure(domain),
+    //             ],
+    //         })
+    //     );
+    //     const resolverResponse2 = await suiProvider.devInspectTransaction({
+    //         sender: sender,
+    //         transaction: resolverTx,
+    //     });
+    //     const addr = get(resolverResponse2, DEV_INSPECT_RESULT_PATH_0);
 
-        if (!addr) return domain;
+    //     if (!addr) return domain;
 
-        return toFullAddress(toHexString(addr));
-    } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log('Error retrieving address from SuiNS name', e);
-        return domain;
-    }
+    //     return toFullAddress(toHexString(addr));
+    // // } catch (e) {
+    // //     // eslint-disable-next-line no-console
+    // //     console.log('Error retrieving address from SuiNS name', e);
+    // //     return domain;
+    // // }
 };
