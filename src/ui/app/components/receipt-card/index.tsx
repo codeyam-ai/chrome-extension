b793bc9eb69a705a4f04f2343c94333124a3d8c7
Adam Wade <adamwade2384@gmail.com>
2023-04-04 21:13:43 -0700

    improve icon display

--- src/ui/app/components/receipt-card/index.tsx
+++ src/ui/app/components/receipt-card/index.tsx
@@ -3,9 +3,13 @@
 
 // import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
 import {
+    ArrowDownCircleIcon,
+    ArrowUpCircleIcon,
     // ArrowDownCircleIcon,
     // ArrowUpCircleIcon,
     ArrowUpRightIcon,
+    CogIcon,
+    SparklesIcon,
     // CogIcon,
     // SparklesIcon,
     // XMarkIcon,
@@ -35,6 +39,10 @@ import type { FormattedTransaction } from '../../helpers/transactions/types';
 import type { SuiTransactionBlockResponse } from '@mysten/sui.js';
 
 import st from './ReceiptCard.module.scss';
+import WalletColorAndEmojiCircle from '../../shared/WalletColorAndEmojiCircle';
+import CopyBody from '../../shared/typography/CopyBody';
+import truncateMiddle from '../../helpers/truncate-middle';
+import { Icon } from '../../shared/icons/Icon';
 
 type TxResponseProps = {
     txDigest: string | null;
@@ -47,53 +55,53 @@ type TxResponseProps = {
 // Truncate text after one line (~ 35 characters)
 // const TRUNCATE_MAX_CHAR = 40;
 
-// const AvatarItem = ({
-//     bgColor,
-//     header,
-//     subheader,
-//     emoji,
-// }: {
-//     bgColor?: string;
-//     header?: string;
-//     subheader?: string;
-//     emoji?: string;
-// }) => (
-//     <div
-//         className={
-//             'p-[10px] flex flex-row space-around items-center align-center gap-4'
-//         }
-//     >
-//         <WalletColorAndEmojiCircle
-//             emojiSizeInPx={20}
-//             circleSizeClasses={'w-[40px] h-[40px] auto'}
-//             color={bgColor || '#7E23CA'}
-//             emoji={emoji}
-//         />
-//         <div className={'flex flex-col items-left'}>
-//             {(header || '').length > 15 ? (
-//                 <CopyBody
-//                     txt={header || ''}
-//                     large
-//                     isSemibold
-//                     className={'text-left'}
-//                 >
-//                     {truncateMiddle(header)}
-//                 </CopyBody>
-//             ) : (
-//                 <BodyLarge isSemibold className={'text-left'}>
-//                     {header}
-//                 </BodyLarge>
-//             )}
+const AvatarItem = ({
+    bgColor,
+    header,
+    subheader,
+    emoji,
+}: {
+    bgColor?: string;
+    header?: string;
+    subheader?: string;
+    emoji?: string;
+}) => (
+    <div
+        className={
+            'p-[10px] flex flex-row space-around items-center align-center gap-4'
+        }
+    >
+        <WalletColorAndEmojiCircle
+            emojiSizeInPx={20}
+            circleSizeClasses={'w-[40px] h-[40px] auto'}
+            color={bgColor || '#7E23CA'}
+            emoji={emoji}
+        />
+        <div className={'flex flex-col items-left'}>
+            {(header || '').length > 15 ? (
+                <CopyBody
+                    txt={header || ''}
+                    large
+                    isSemibold
+                    className={'text-left'}
+                >
+                    {truncateMiddle(header)}
+                </CopyBody>
+            ) : (
+                <BodyLarge isSemibold className={'text-left'}>
+                    {header}
+                </BodyLarge>
+            )}
 
-//             <CopyBody
-//                 txt={subheader || ''}
-//                 className={'text-ethos-light-text-medium text-left'}
-//             >
-//                 {truncateMiddle(subheader)}
-//             </CopyBody>
-//         </div>
-//     </div>
-// );
+            <CopyBody
+                txt={subheader || ''}
+                className={'text-ethos-light-text-medium text-left'}
+            >
+                {truncateMiddle(subheader)}
+            </CopyBody>
+        </div>
+    </div>
+);
 
 // const TxTransfer = ({
 //     ToFrom,
@@ -192,6 +200,7 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
     if (!transaction) return <div>Transaction not found.</div>;
 
     const {
+        txAction,
         timeDisplay,
         txType,
         txAmount,
@@ -200,6 +209,7 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
         gasFeeInSui,
         txCommands,
         displayImage,
+        otherAddressStr,
     } = getHumanReadable(address, transaction);
 
     /*const getAccount = useCallback(
@@ -209,9 +219,9 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
             );
         },
         [accountInfos]
-    );
+    );*/
 
-    const fromWallet = useMemo(
+    /*const fromWallet = useMemo(
         () => (txDigest.from ? getAccount(txDigest.from) : null),
         [getAccount, txDigest]
     );
@@ -222,27 +232,6 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
     );
 
     const [searchParams] = useSearchParams();
-    const isFunc = searchParams.get('isFunc');
-
-    const theme = getTheme();
-
-    const toAddrStr = useMiddleEllipsis(
-        txDigest.to || '',
-        TRUNCATE_MAX_LENGTH,
-        TRUNCATE_PREFIX_LENGTH
-    );
-
-    const fromAddrStr = useMiddleEllipsis(
-        txDigest.from || '',
-        TRUNCATE_MAX_LENGTH,
-        TRUNCATE_PREFIX_LENGTH
-    );
-
-    const truncatedNftName = useMiddleEllipsis(
-        txDigest?.name || '',
-        TRUNCATE_MAX_CHAR,
-        TRUNCATE_MAX_CHAR - 1
-    );
 
     const [gas] = useFormatCoin(txDigest.txGas, SUI_TYPE_ARG);
 
@@ -263,70 +252,63 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
           ])
         : false;
 
-    const isNft = typeof txDigest?.url !== 'undefined';
-    const coinType = txDigest?.kind === 'PaySui' ? 'SUI' : 'Coin';
-    const transferType =
-        txDigest?.kind === 'Call'
-            ? 'Call'
-            : txDigest.isSender
-            ? 'Sent'
-            : 'Received';
+    const coinType = txDigest?.kind === 'PaySui' ? 'SUI' : 'Coin';*/
 
-    const header =
-        _.startCase(txDigest?.name) ||
-        _.startCase(truncatedNftName) ||
-        _.startCase(txDigest.objSymbol) ||
-        _.startCase(txDigest.callModuleName);
-    const isMinted = txDigest?.callFunctionName === 'mint';
+    let transferObj;
+    let transferAction = txAction;
 
-    const transferMeta = {
-        Call: {
-            txName: isMinted
-                ? 'Minted'
-                : `Call ${
-                      txDigest?.callFunctionName &&
-                      '(' + txDigest?.callFunctionName + ')'
-                  }`,
-            txIcon: isMinted ? (
-                <Icon
-                    isRound={header === 'Coin'}
-                    displayIcon={<SparklesIcon />}
-                />
-            ) : (
-                <Icon displayIcon={<CogIcon />} />
-            ),
-            transfer: txDigest?.isSender ? 'To' : 'From',
-            address: false,
-            addressTruncate: false,
-            failedMsg: txDigest?.error || 'Failed',
-        },
-        Sent: {
-            txName: 'Sent',
-            transfer: 'To',
-            txIcon: (
-                <Icon
-                    displayIcon={<ArrowUpCircleIcon />}
-                    isRound={isNft ? false : true}
-                />
-            ),
-            addressTruncate: toAddrStr,
-            address: txDigest.to,
-            failedMsg: txDigest?.error || 'Failed',
-        },
-        Received: {
-            txName: 'Received',
-            transfer: 'From',
-            txIcon: (
-                <Icon
-                    displayIcon={<ArrowDownCircleIcon />}
-                    isRound={isNft ? false : true}
-                />
-            ),
-            addressTruncate: fromAddrStr,
-            address: txDigest.from,
-            failedMsg: '',
-        },
-    };*/
+    switch (transferAction) {
+        case 'send':
+            transferObj = {
+                txName: 'Sent',
+                transfer: 'To',
+                txIcon: (
+                    <Icon
+                        displayIcon={<ArrowUpCircleIcon />}
+                        isRound={txType === 'nft' ? false : true}
+                    />
+                ),
+                addressTruncate: 'get-to-str',
+                address: 'get-to-str',
+                failedMsg: txStatus === 'success' || 'Failed',
+            };
+            break;
+        case 'receive':
+            transferObj = {
+                txName: 'Received',
+                transfer: 'From',
+                txIcon: (
+                    <Icon
+                        displayIcon={<ArrowDownCircleIcon />}
+                        isRound={txType === 'nft' ? false : true}
+                    />
+                ),
+                addressTruncate: truncateMiddle(otherAddressStr, 5),
+                address: otherAddressStr,
+                failedMsg: '',
+            };
+            break;
+        default:
+            transferObj = {
+                txName: txAction === 'mint' ? 'Minted' : `${txAction}`,
+                txIcon:
+                    txAction === 'mint' ? (
+                        <Icon
+                            isRound={txType === 'coin' || txType === 'sui'}
+                            displayIcon={<SparklesIcon />}
+                        />
+                    ) : (
+                        <Icon displayIcon={<CogIcon />} />
+                    ),
+                transfer: txAction === 'send' ? 'To' : 'From',
+                address: false,
+                addressTruncate: false,
+                failedMsg: txStatus === 'success' || 'Failed',
+            };
+            break;
+    }
+
+    console.log('transaction', txRef);
 
     return (
         <>
@@ -337,11 +319,11 @@ function ReceiptCard({ txDigest }: TxResponseProps) {
                     coinType={'SUI'} // TODO: handle coin types
                     imgUrl={displayImage || ''}
                     name={txCommands || 'NFT'}
-                    icon={<></>} // TODO: handle success / failure icon
+                    icon={transferObj.txIcon} // TODO: handle success / failure icon
                 />
                 <Body className={'text-ethos-light-text-medium'}>
                     {txStatus === 'success'
-                        ? _.toUpper(txType)
+                        ? `${_.capitalize(txAction)} ${_.toUpper(txType)}`
                         : 'Transaction Failed'}
                 </Body>
                 <Header className={'font-weight-ethos-subheader mb-3'}>
