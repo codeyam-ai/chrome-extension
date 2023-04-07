a5dcbf93081fcfb1969f0bd1c77762c69357ede5
Mike Grafton <2266+mag@users.noreply.github.com>
2023-04-07 12:12:48 -0700

    Must change password to a strong one (#127)

---
+++ src/ui/app/shared/forms/validation-utils.ts
@@ -0,0 +1,23 @@
+import * as Yup from "yup";
+import zxcvbn from "zxcvbn";
+
+export function passwordComplexityValidation(requiredText: string) {
+    return Yup.string()
+        .ensure()
+        .required(requiredText)
+        .test({
+            name: 'password-strength',
+            test: (password: string) => {
+                return zxcvbn(password).score > 2;
+            },
+            message: ({ value }) => {
+                const {
+                    feedback: { warning, suggestions },
+                } = zxcvbn(value);
+                const warn =
+                    (warning && `${warning}.`) || 'Password is not strong enough.';
+                return `${warn} ${suggestions.join(' ')}`;
+            },
+        });
+
+}
