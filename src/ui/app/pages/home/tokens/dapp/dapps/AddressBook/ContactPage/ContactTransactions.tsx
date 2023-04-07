f8cee50cc91172871946660519299c102fab925e
Adam Wade <adamwade2384@gmail.com>
2023-04-06 23:33:50 -0700

    Fix hook for contact transactions

--- src/ui/app/pages/home/tokens/dapp/dapps/AddressBook/ContactPage/ContactTransactions.tsx
+++ src/ui/app/pages/home/tokens/dapp/dapps/AddressBook/ContactPage/ContactTransactions.tsx
@@ -2,10 +2,10 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import Loading from '_src/ui/app/components/loading';
 import { useAppSelector } from '_src/ui/app/hooks';
-import { useQueryTransactionsByAddress } from '_src/ui/app/hooks/useQueryTransactionsByAddress';
+import { queryTransactionsByAddress } from '_src/ui/app/hooks/useQueryTransactionsByAddress';
 import TransactionRows from '_src/ui/app/shared/content/rows-and-lists/TransactionRows';
 import Subheader from '_src/ui/app/shared/typography/Subheader';
-
+import { useQuery } from '@tanstack/react-query';
 import type { FormattedTransaction } from '_src/ui/app/helpers/transactions/types';
 
 interface ContactTransactionsProps {
@@ -25,11 +25,27 @@ const ContactTransactions: React.FC<ContactTransactionsProps> = ({
             return txn.humanReadable.addresses?.to === contactAddress;
         });
     }, [formattedTxns, contactAddress]);
+
+    const fetchTransactions = async () => {
+        const response = await queryTransactionsByAddress(
+            userAddress || '',
+            null,
+            null,
+            null
+        );
+        return response;
+    };
+
     const {
-        isLoading: loadingTxns,
+        isFetching: loadingTxns,
         data: suiTxns,
         // error: txErr,
-    } = useQueryTransactionsByAddress(userAddress || '', null, null, null);
+    } = useQuery(['contact-transactions'], fetchTransactions, {
+        enabled: !!userAddress,
+        staleTime: 0,
+        cacheTime: 0,
+        refetchInterval: 3000,
+    });
 
     const loadPage = useCallback(async () => {
         if (!suiTxns) return;
