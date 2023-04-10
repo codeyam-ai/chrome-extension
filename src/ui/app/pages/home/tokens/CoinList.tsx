d7576c3761780eac503e540ae19e27c99d68e999
Adam Wade <adamwade2384@gmail.com>
2023-04-10 12:17:10 -0700

    cleanup coinlist component and move loading to parent

--- src/ui/app/pages/home/tokens/CoinList.tsx
+++ src/ui/app/pages/home/tokens/CoinList.tsx
@@ -1,6 +1,5 @@
 import CoinBalance from './CoinBalance';
 import Loading from '_src/ui/app/components/loading';
-import Subheader from '_src/ui/app/shared/typography/Subheader';
 
 const CoinList = ({ balances }: { balances: Record<string, bigint> }) => {
     if (Object.keys(balances).length === 0) {
@@ -8,36 +7,16 @@ const CoinList = ({ balances }: { balances: Record<string, bigint> }) => {
     }
     return (
         <div className="text-left">
-            <Subheader>Coins</Subheader>
-            <Loading
-                className="py-3 mt-3 flex justify-center items-center"
-                loading={!balances}
-            >
-                {Object.keys(balances).map((type: string) => {
-                    const balance = balances[type];
-                    return (
-                        <div className={'mt-3'} key={type}>
-                            <CoinBalance type={type} balance={balance} />
-                        </div>
-                    );
-                })}
-            </Loading>
+            {Object.keys(balances).map((type: string) => {
+                const balance = balances[type];
+                return (
+                    <div className={'my-3'} key={type}>
+                        <CoinBalance type={type} balance={balance} />
+                    </div>
+                );
+            })}
         </div>
     );
 };
 
 export default CoinList;
-
-// otherCoinTypes.map((aCoinType) => {
-//     const aCoinBalance = balances[aCoinType];
-//     return (
-//         <>
-//             <div className="">OTHER COINS</div>
-//             <CoinBalance
-//                 type={aCoinType}
-//                 balance={aCoinBalance}
-//                 key={aCoinType}
-//             />
-//         </>
-//     );
-// });
