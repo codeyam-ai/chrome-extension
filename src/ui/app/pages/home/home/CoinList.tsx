1521f5cd832a056ebefe68e2264e6daeb5061f62
Adam Wade <adamwade2384@gmail.com>
2023-04-10 16:00:19 -0700

    adjust spacing

--- src/ui/app/pages/home/home/CoinList.tsx
+++ src/ui/app/pages/home/home/CoinList.tsx
@@ -1,16 +1,12 @@
 import CoinBalance from './CoinBalance';
-import Loading from '_src/ui/app/components/loading';
 
 const CoinList = ({ balances }: { balances: Record<string, bigint> }) => {
-    if (Object.keys(balances).length === 0) {
-        return <></>;
-    }
     return (
         <div className="text-left">
             {Object.keys(balances).map((type: string) => {
                 const balance = balances[type];
                 return (
-                    <div className={'my-3'} key={type}>
+                    <div className={'mb-5'} key={type}>
                         <CoinBalance type={type} balance={balance} />
                     </div>
                 );
