109e0424b2cfa42d2d2580a768fa46c23733d6ba
Kevin Jasker <kevinjasker@gmail.com>
2023-04-10 22:04:50 -0700

    feat: add copy-able address to tokens home
    [finishes ETHOS-639]

--- src/ui/app/pages/home/home/WalletBalanceAndIconHomeView.tsx
+++ src/ui/app/pages/home/home/WalletBalanceAndIconHomeView.tsx
@@ -1,4 +1,9 @@
+import { Square2StackIcon } from '@heroicons/react/24/outline';
+
+import CopyToClipboard from '_src/ui/app/components/copy-to-clipboard';
+import useMiddleEllipsis from '_src/ui/app/hooks/useMiddleEllipsis';
 import WalletColorAndEmojiCircle from '_src/ui/app/shared/WalletColorAndEmojiCircle';
+import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
 import JumboTitle from '_src/ui/app/shared/typography/JumboTitle';
 
@@ -16,6 +21,13 @@ const WalletBalanceAndIconHomeView = ({
     if (dollarValue.endsWith('.00')) {
         dollarValue = dollarValue.slice(0, -3);
     }
+
+    const addressWithEllipsis = useMiddleEllipsis(
+        accountInfo?.address || '',
+        9,
+        5
+    );
+
     return (
         <div className="flex flex-col gap-3 place-items-center">
             <div className="p-3">
@@ -31,6 +43,21 @@ const WalletBalanceAndIconHomeView = ({
                     Wallet Balance
                 </BodyLarge>
                 <JumboTitle>{dollarValue}</JumboTitle>
+
+                {addressWithEllipsis && (
+                    <div className="pb-4">
+                        <CopyToClipboard txt={accountInfo?.address || ''}>
+                            <Body>{addressWithEllipsis}</Body>
+                            <div className="p-2">
+                                <Square2StackIcon
+                                    className="text-ethos-light-primary-light"
+                                    width={14}
+                                    height={14}
+                                />
+                            </div>
+                        </CopyToClipboard>
+                    </div>
+                )}
             </div>
         </div>
     );
