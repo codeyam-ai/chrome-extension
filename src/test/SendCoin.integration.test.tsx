d69b76de85828923be57eadeab4e5138a91895df
Adam Wade <adamwade2384@gmail.com>
2023-04-11 11:24:36 -0700

    auto formatting

--- src/test/SendCoin.integration.test.tsx
+++ src/test/SendCoin.integration.test.tsx
@@ -31,7 +31,7 @@ describe('send coin flow', () => {
     const shouldSeeRootPageAndClickSend = async () => {
         // await screen.findByText('Coins');
         // await screen.findByText('Get started with Sui');
-        console.log(document)
+        console.log(document);
         const sendButton = await screen.findByText('Send');
         await userEvent.click(sendButton);
     };
