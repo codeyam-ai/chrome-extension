056501d518f2894ab0c9e1ef7fce9eda96b4bbac
Adam Wade <adamwade2384@gmail.com>
2023-04-11 16:09:46 -0700

    working on select validator

--- src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
@@ -1,6 +1,8 @@
 import Subheader from '_src/ui/app/shared/typography/Subheader';
 import EthosLink from '_src/ui/app/shared/typography/EthosLink';
 import { LinkType } from '_src/enums/LinkType';
+import Button from '_src/ui/app/shared/buttons/Button';
+import { useCallback } from 'react';
 
 interface ValidatorSelectProps {
     validator?: string;
@@ -13,24 +15,40 @@ const ValidatorSelect = ({
     apy,
     isHighReward,
 }: ValidatorSelectProps) => {
+    const selectValidator = useCallback(() => {
+        console.log(validator);
+    }, []);
+
     return (
-        <div
+        <button
+            onClick={selectValidator}
             className={
-                'py-4 px-3 text-left border-1 border-color-ethos-light-text-medium dark:border-color-ethos-dark-secondary'
+                'block w-full mb-5 rounded-md py-4 px-3 text-left border hover:border hover:dark:border-ethos-dark-primary-dark border-ethos-light-text-medium dark:border-ethos-dark-text-stroke transition-all'
             }
         >
             <div className={'flex flex-col justify-between relative'}>
                 {isHighReward && (
-                    <div className={'absolute top-0 right-0'}>
+                    <div
+                        className={
+                            'absolute text-white top-[-27px] right-2 text-sm p-1 bg-[#01C57E] rounded-md'
+                        }
+                    >
                         Highest Reward
                     </div>
                 )}
-                <div className={'mb-8 text-md'}>Coinbase</div>
-                <div className={'text-xl font-bold'}>
-                    5.5% <span className={'text'}>APY</span>
+                <div className={'mb-6 text-lg'}>{validator}</div>
+                <div className={'text-2xl font-bold'}>
+                    {apy}%{' '}
+                    <span
+                        className={
+                            'text-lg text-ethos-light-text-medium dark:text-ethos-dark-text-secondary'
+                        }
+                    >
+                        APY
+                    </span>
                 </div>
             </div>
-        </div>
+        </button>
     );
 };
 
@@ -38,18 +56,31 @@ const SelectValidator: React.FC = () => {
     // add component
     return (
         <div className={'p-6 relative'}>
-            <Subheader className={'text-center'}>
+            <Subheader className={'text-center mb-1'}>
                 Select a staking validator
             </Subheader>
             <EthosLink
-                className={'font-height-[24px] underline'}
+                className={'text-sm underline'}
                 to={'/home/staking/learn-more'}
                 type={LinkType.Internal}
             >
                 What is Staking?
             </EthosLink>
-            <ValidatorSelect isHighReward={true} />
-            <ValidatorSelect isHighReward={false} />
+            <div className={'mb-6 mt-10'}>
+                <ValidatorSelect
+                    isHighReward={true}
+                    validator={'Coinbase'}
+                    apy={'5'}
+                />
+                <ValidatorSelect
+                    isHighReward={false}
+                    validator={'Everstake'}
+                    apy={'4.4'}
+                />
+            </div>
+            <Button to={'/home/staking/staking-amount'} removeContainerPadding>
+                Next
+            </Button>
         </div>
     );
 };
