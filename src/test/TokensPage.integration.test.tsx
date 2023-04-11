1cbf14e6bc37e75ce9e98acbb8af803cc3772de1
Mike Grafton <2266+mag@users.noreply.github.com>
2023-04-11 12:44:08 -0700

    Move locking test to its own file

--- src/test/TokensPage.integration.test.tsx
+++ src/test/TokensPage.integration.test.tsx
@@ -1,6 +1,5 @@
-import { act, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
-import { fakeAlarms } from '_src/test/utils/fake-browser/fake-browser';
 import { Mockchain } from '_src/test/utils/mockchain';
 import { renderApp } from '_src/test/utils/react-rendering';
 import { simulateMnemonicUser } from '_src/test/utils/storage';
@@ -25,43 +24,5 @@ describe('Rendering the Tokens page', () => {
         });
         renderApp();
         await screen.findByText('$4,000');
-    });
-
-    class FakeHeartbeat {
-        capturedListener?: () => void;
-        onBeat(listener: () => void) {
-            this.capturedListener = listener;
-        }
-    }
-
-    test('sends heartbeat and locks when background service says to', async () => {
-        const fakeHeartbeat = new FakeHeartbeat();
-        mockchain.mockSuiObjects();
-        renderApp({
-            dependencies: {
-                closeWindow: jest.fn(),
-                heartbeat: fakeHeartbeat,
-            },
-        });
-        await screen.findByText('Get started with Sui');
-
-        // at this point we expect the heartbeat listener to be registered but no alarm to be set yet
-        expect(fakeHeartbeat.capturedListener).not.toBeNull();
-        expect(fakeAlarms.names).not.toContain('lockAlarm');
-
-        // this sends the heartbeat to the background task
-        fakeHeartbeat.capturedListener && fakeHeartbeat.capturedListener();
-        expect(fakeAlarms.names).toContain('lockAlarm');
-
-        // now invoke the alarm, which should trigger the UI to lock
-        act(() => {
-            fakeAlarms.onAlarm.listeners[0]({
-                name: 'lockAlarm',
-                periodInMinutes: 0,
-                scheduledTime: 1,
-            });
-        });
-
-        await screen.findAllByText('Unlock Wallet');
     });
 });
