7e192930be05e4cbb49c35474a234fddf3a9fda7
Kevin Jasker <kevinjasker@gmail.com>
2023-04-12 12:46:57 -0700

    fix validator select next button positioning

--- src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
@@ -9,21 +9,27 @@ import ValidatorList from './ValidatorList';
 const SelectValidator: React.FC = () => {
     // add component
     return (
-        <div className={'p-6 relative'}>
-            <Subheader className={'text-center mb-1'}>
-                Select a staking validator
-            </Subheader>
-            <EthosLink
-                className={'text-sm underline'}
-                to={'/home/staking/learn-more'}
-                type={LinkType.Internal}
-            >
-                What is Staking?
-            </EthosLink>
-            <div className={'mb-6 mt-10'}>
-                <ValidatorList />
+        <div className={'flex flex-col p-6 h-[414px] relative'}>
+            <div className={'overflow-y-scroll h-full no-scrollbar'}>
+                <Subheader className={'text-center mb-1'}>
+                    Select a staking validator
+                </Subheader>
+                <EthosLink
+                    className={'text-sm underline'}
+                    to={'/home/staking/learn-more'}
+                    type={LinkType.Internal}
+                >
+                    What is Staking?
+                </EthosLink>
+                <div className="mb-6 mt-10">
+                    <ValidatorList />
+                </div>
             </div>
-            <Button to={'/home/staking/amount-to-stake'} removeContainerPadding>
+            <Button
+                to={'/home/staking/amount-to-stake'}
+                removeContainerPadding
+                className="!mt-2"
+            >
                 Next
             </Button>
         </div>
