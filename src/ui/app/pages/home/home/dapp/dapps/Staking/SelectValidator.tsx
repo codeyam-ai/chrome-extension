6080e7eef1511ca6c4983ec8cabb96080337da68
Kevin Jasker <kevinjasker@gmail.com>
2023-04-12 13:07:03 -0700

    enable validator selection from validator list

--- src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/SelectValidator.tsx
@@ -2,12 +2,22 @@ import Subheader from '_src/ui/app/shared/typography/Subheader';
 import EthosLink from '_src/ui/app/shared/typography/EthosLink';
 import { LinkType } from '_src/enums/LinkType';
 import Button from '_src/ui/app/shared/buttons/Button';
-import { useCallback } from 'react';
-import { SuiValidatorSummary } from '@mysten/sui.js';
+import { useCallback, useState } from 'react';
+import { SuiAddress, SuiValidatorSummary } from '@mysten/sui.js';
 import ValidatorList from './ValidatorList';
 
 const SelectValidator: React.FC = () => {
-    // add component
+    const [selectedValidator, setSelectedValidator] = useState<
+        SuiAddress | undefined
+    >();
+
+    const onSelectValidator = useCallback(
+        (validatorAddress: SuiAddress) => {
+            setSelectedValidator(validatorAddress);
+        },
+        [setSelectedValidator]
+    );
+
     return (
         <div className={'flex flex-col p-6 h-[414px] relative'}>
             <div className={'overflow-y-scroll h-full no-scrollbar'}>
@@ -22,13 +32,17 @@ const SelectValidator: React.FC = () => {
                     What is Staking?
                 </EthosLink>
                 <div className="mb-6 mt-10">
-                    <ValidatorList />
+                    <ValidatorList
+                        selectedValidator={selectedValidator}
+                        onSelectValidator={onSelectValidator}
+                    />
                 </div>
             </div>
             <Button
                 to={'/home/staking/amount-to-stake'}
                 removeContainerPadding
                 className="!mt-2"
+                disabled={!selectedValidator}
             >
                 Next
             </Button>
--- src/ui/app/pages/home/home/dapp/dapps/Staking/ValidatorList.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/ValidatorList.tsx
@@ -1,11 +1,19 @@
-import { SuiValidatorSummary } from '@mysten/sui.js';
+import { SuiAddress, SuiValidatorSummary } from '@mysten/sui.js';
 import { useQuery } from '@tanstack/react-query';
 import truncateMiddle from '_src/ui/app/helpers/truncate-middle';
 import { api } from '_src/ui/app/redux/store/thunk-extras';
 import Body from '_src/ui/app/shared/typography/Body';
-import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useCallback, useEffect, useState } from 'react';
 
-const ValidatorList: React.FC = () => {
+interface ValidatorListProps {
+    onSelectValidator: (string: SuiAddress) => void;
+    selectedValidator?: SuiAddress;
+}
+const ValidatorList: React.FC<ValidatorListProps> = ({
+    onSelectValidator,
+    selectedValidator,
+}) => {
     const [validators, setValidators] = useState<SuiValidatorSummary[]>([]);
 
     useEffect(() => {
@@ -13,7 +21,6 @@ const ValidatorList: React.FC = () => {
         const fetchValidators = async () => {
             const provider = api.instance.fullNode;
             const res = await provider.getLatestSuiSystemState();
-            console.log('res :>> ', res.activeValidators);
             setValidators(res.activeValidators);
         };
         fetchValidators();
@@ -22,19 +29,35 @@ const ValidatorList: React.FC = () => {
     return (
         <div className="flex flex-col">
             {validators.map((validator, key) => (
-                <ValidatorRow validator={validator} key={key} />
+                <ValidatorRow
+                    onSelect={onSelectValidator}
+                    validator={validator}
+                    key={key}
+                    isSelected={validator.suiAddress === selectedValidator}
+                />
             ))}
         </div>
     );
 };
 
-const ValidatorRow: React.FC<{ validator: SuiValidatorSummary }> = ({
-    validator,
-}) => {
+const ValidatorRow: React.FC<{
+    validator: SuiValidatorSummary;
+    onSelect: (suiAddress: SuiAddress) => void;
+    isSelected: boolean;
+}> = ({ validator, onSelect, isSelected }) => {
+    const onClick = useCallback(() => {
+        onSelect(validator.suiAddress);
+    }, [onSelect, validator]);
+
     return (
         <button
-            // onClick={selectValidator}
-            className="w-full py-3 px-2 text-left rounded-lg border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke"
+            onClick={onClick}
+            className={classNames(
+                'w-full py-3 px-2 text-left rounded-lg border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke',
+                isSelected
+                    ? 'border-2 border-b-2 border-ethos-light-primary-light dark:border-ethos-dark-primary-dark'
+                    : ''
+            )}
         >
             <div className="flex flex-row items-center place-content-center justify-between">
                 <div className="flex items-center place-content-center gap-3">
