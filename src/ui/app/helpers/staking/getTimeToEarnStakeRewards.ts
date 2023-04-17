10cf1f28c720c5d8e0afb7c2449fefa0371d0bad
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-04-17 15:18:21 -0700

    add calculation for timeToEarnStakeRewards

---
+++ src/ui/app/helpers/staking/getTimeToEarnStakeRewards.ts
@@ -0,0 +1,22 @@
+import type { SuiSystemStateSummary } from '@mysten/sui.js';
+
+const getTimeToEarnStakingRewards = (
+    systemState: SuiSystemStateSummary,
+    startEarningEpoch: number
+) => {
+    const currentEpoch = Number(systemState.epoch || 0);
+    const currentEpochStartTime = Number(
+        systemState.epochStartTimestampMs || 0
+    );
+    const epochPeriod = Number(systemState.epochDurationMs || 0);
+    const timeBeforeSpecifiedEpoch =
+        startEarningEpoch > currentEpoch &&
+        startEarningEpoch > 0 &&
+        epochPeriod > 0
+            ? currentEpochStartTime +
+              (startEarningEpoch - currentEpoch) * epochPeriod
+            : 0;
+    return timeBeforeSpecifiedEpoch;
+};
+
+export default getTimeToEarnStakingRewards;
--- src/ui/app/pages/home/home/dapp/dapps/Staking/StakeAmount/StakeAmountForm.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/StakeAmount/StakeAmountForm.tsx
@@ -1,13 +1,20 @@
 import { Form, useField, useFormikContext } from 'formik';
+import { useEffect, useState } from 'react';
 
 import StakeSummary from '../StakeSummary';
 import { type SuiValidatorSummaryWithApy } from '../ValidatorList';
+import { NUM_OF_EPOCH_BEFORE_EARNING } from '_src/shared/constants';
+import getTimeToEarnStakingRewards from '_src/ui/app/helpers/staking/getTimeToEarnStakeRewards';
+import truncateMiddle from '_src/ui/app/helpers/truncate-middle';
 import mistToSui from '_src/ui/app/pages/dapp-tx-approval/lib/mistToSui';
+import { api } from '_src/ui/app/redux/store/thunk-extras';
 import Button from '_src/ui/app/shared/buttons/Button';
 import SuiIcon from '_src/ui/app/shared/svg/SuiIcon';
 import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
 import Subheader from '_src/ui/app/shared/typography/Subheader';
+
+import type { SuiSystemStateSummary } from '@mysten/sui.js';
 
 interface StakeAmountFormProps {
     validator: SuiValidatorSummaryWithApy;
@@ -18,9 +25,30 @@ const StakeAmountForm: React.FC<StakeAmountFormProps> = ({
     validator,
     formattedSuiBalance,
 }) => {
-    const { isSubmitting, isValid, dirty } = useFormikContext();
-
+    const [epoch, setEpoch] = useState(0);
     const [amountField, amountMeta] = useField('amount');
+    const [systemState, setSystemState] = useState<
+        SuiSystemStateSummary | undefined
+    >();
+    // Reward will be available after 2 epochs
+    const startEarningRewardsEpoch =
+        Number(epoch || 0) + NUM_OF_EPOCH_BEFORE_EARNING;
+
+    const { isSubmitting, isValid, dirty } = useFormikContext();
+    const timeToEarnStakeRewards =
+        systemState &&
+        getTimeToEarnStakingRewards(systemState, startEarningRewardsEpoch);
+    console.log('timeToEarnStakeRewards :>> ', timeToEarnStakeRewards);
+
+    useEffect(() => {
+        const getSystemState = async () => {
+            const provider = api.instance.fullNode;
+            const data = await provider.getLatestSuiSystemState();
+            setSystemState(data);
+            setEpoch(Number(data.epoch));
+        };
+        getSystemState();
+    }, []);
 
     return (
         <Form
@@ -31,6 +59,34 @@ const StakeAmountForm: React.FC<StakeAmountFormProps> = ({
                 <Subheader className={'text-center mb-6'}>
                     How much would you like to stake?
                 </Subheader>
+                <div className="flex justify-between p-3 rounded-lg bg-ethos-light-background-secondary dark:bg-ethos-dark-background-secondary">
+                    <div className="flex items-center place-content-center gap-3">
+                        {validator.imageUrl ? (
+                            <img
+                                src={validator.imageUrl}
+                                alt={validator.name}
+                                className="h-9 w-9 rounded-full"
+                            />
+                        ) : (
+                            <div className="h-9 w-9 rounded-full bg-ethos-light-background-secondary dark:bg-ethos-dark-background-secondary" />
+                        )}
+                        <div className="flex flex-col items-start">
+                            <Body isSemibold>{validator.name}</Body>
+                            <Body>{validator.description}</Body>
+                            <Body isTextColorMedium>
+                                {truncateMiddle(validator.suiAddress)}
+                            </Body>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-end">
+                        <Body isSemibold>Staking Share</Body>
+                        <Body isTextColorMedium>?</Body>
+                    </div>
+                    <div className="flex flex-col items-end">
+                        <Body isSemibold>Total Staked</Body>
+                        <Body isTextColorMedium>?</Body>
+                    </div>
+                </div>
                 <div className="flex flex-col gap-2 py-5 px-4 rounded-xl border border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke">
                     <div className="flex justify-between">
                         <Body isSemibold>Amount</Body>
