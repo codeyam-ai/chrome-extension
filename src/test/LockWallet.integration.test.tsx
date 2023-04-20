5a2e0fbf765a9d62cb0892ea25ea36efd4135560
Mike Grafton <2266+mag@users.noreply.github.com>
2023-04-20 15:39:16 -0700

    Add a test for when an email user locks wallet

--- src/test/LockWallet.integration.test.tsx
+++ src/test/LockWallet.integration.test.tsx
@@ -3,33 +3,59 @@ import userEvent from '@testing-library/user-event';
 
 import { Mockchain } from '_src/test/utils/mockchain';
 import { renderApp } from '_src/test/utils/react-rendering';
-import { simulateMnemonicUser } from '_src/test/utils/storage';
+import {
+    simulateEmailUser,
+    simulateMnemonicUser,
+} from '_src/test/utils/storage';
+import { before } from 'node:test';
 
 describe('Lock Wallet Page', () => {
     let mockchain: Mockchain;
 
-    beforeEach(() => {
-        mockchain = new Mockchain();
-    });
-
     beforeEach(async () => {
-        simulateMnemonicUser();
+        mockchain = new Mockchain();
         mockchain.mockCommonCalls();
         mockchain.mockSuiObjects();
     });
 
-    test('allows user to lock the wallet manually', async () => {
-        await renderApp();
-        await screen.findByText('Get started with Sui');
-        const settingsButton = await screen.findByTestId('settings-toggle');
-        await userEvent.click(settingsButton);
+    describe('for an email user', () => {
+        beforeEach(async () => {
+            simulateEmailUser();
+        });
 
-        const lockOption = await screen.findByText('Lock Ethos');
-        await userEvent.click(lockOption);
+        test('allows user to lock the wallet manually', async () => {
+            await renderApp();
+            await screen.findByText('Get started with Sui');
+            const settingsButton = await screen.findByTestId('settings-toggle');
+            await userEvent.click(settingsButton);
 
-        const lockWalletButton = await screen.findByText('Lock Wallet');
-        await userEvent.click(lockWalletButton);
+            const lockOption = await screen.findByText('Lock Ethos');
+            await userEvent.click(lockOption);
 
-        await screen.findAllByText('Unlock Wallet');
+            const lockWalletButton = await screen.findByText('Lock Wallet');
+            await userEvent.click(lockWalletButton);
+
+            await screen.findAllByText('Sign in with Email');
+        });
+    });
+
+    describe('for a mnemonic user', () => {
+        beforeEach(async () => {
+            simulateMnemonicUser();
+        });
+        test('allows user to lock the wallet manually', async () => {
+            await renderApp();
+            await screen.findByText('Get started with Sui');
+            const settingsButton = await screen.findByTestId('settings-toggle');
+            await userEvent.click(settingsButton);
+
+            const lockOption = await screen.findByText('Lock Ethos');
+            await userEvent.click(lockOption);
+
+            const lockWalletButton = await screen.findByText('Lock Wallet');
+            await userEvent.click(lockWalletButton);
+
+            await screen.findAllByText('Unlock Wallet');
+        });
     });
 });
