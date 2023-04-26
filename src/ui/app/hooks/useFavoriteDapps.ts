87fb9795d168b9b65a813c84ed6097c11f96f7ed
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-26 11:40:43 -0400

    load favorites from nfts

--- src/ui/app/hooks/useFavoriteDapps.ts
+++ src/ui/app/hooks/useFavoriteDapps.ts
@@ -1,8 +1,13 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
-import dappsMap from '_src/data/dappsMap';
+import getDisplay from '../helpers/getDisplay';
+import capyart from '_images/dapps/logos/capyart.png';
+import dappsMap, { CUSTOMIZE_ID, NetworkName } from '_src/data/dappsMap';
 import { useAppDispatch, useAppSelector } from '_src/ui/app/hooks';
-import { saveFavoriteDappsKeys } from '_src/ui/app/redux/slices/account';
+import {
+    accountNftsSelector,
+    saveFavoriteDappsKeys,
+} from '_src/ui/app/redux/slices/account';
 
 import type { DappData } from '_src/types/DappData';
 
@@ -11,9 +16,68 @@ export const useFavoriteDapps = () => {
         ({ account }) => account.favoriteDappsKeys
     );
 
-    const favoriteDapps: DappData[] = favoriteDappsKeys
-        .map((key) => dappsMap.get(key))
-        .filter(Boolean) as DappData[];
+    const nfts = useAppSelector(accountNftsSelector);
+
+    const favoriteDapps: DappData[] = useMemo(() => {
+        const allFavoriteDappsKeys = [...favoriteDappsKeys];
+        if (!favoriteDappsKeys.includes(CUSTOMIZE_ID)) {
+            allFavoriteDappsKeys.push(CUSTOMIZE_ID);
+        }
+
+        let projectNFTs: Record<string, DappData> = {};
+        if (nfts) {
+            projectNFTs = nfts
+                .map((nft) => ({ ...(getDisplay(nft.display) ?? {}) }))
+                .filter((display) => {
+                    if (!display) return false;
+
+                    if (
+                        display.project_url === 'https://testnet.suifrens.com'
+                    ) {
+                        display.project_name = 'SuiFrens';
+                        display.project_image_url = capyart;
+                    }
+
+                    return (
+                        display &&
+                        display.project_url &&
+                        display.project_name &&
+                        display.project_image_url
+                    );
+                })
+                .reduce((acc, display) => {
+                    if (!display?.project_url) return acc;
+                    return {
+                        ...acc,
+                        [display.project_url]: {
+                            id: display.project_url,
+                            title: display.project_name,
+                            description: display.project_description,
+                            image: display.project_image_url,
+                            urls: {
+                                [NetworkName.DEVNET]: display.project_url,
+                                [NetworkName.TESTNET]: display.project_url,
+                            },
+                            tags: [],
+                        },
+                    };
+                }, {} as Record<string, DappData>);
+        }
+
+        const dapps = allFavoriteDappsKeys
+            .map((key) => dappsMap.get(key))
+            .filter(Boolean) as DappData[];
+
+        if (Object.values(projectNFTs).length > 0) {
+            Object.keys(projectNFTs).forEach((key) => {
+                if (!dapps.find((d) => d.id === key)) {
+                    dapps.splice(0, 0, projectNFTs[key]);
+                }
+            });
+        }
+
+        return dapps;
+    }, [favoriteDappsKeys, nfts]);
 
     const dispatch = useAppDispatch();
 
