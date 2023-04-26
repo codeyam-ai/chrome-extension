6274a58e2244c44e32600e9077e9e0d11c3b0abd
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-26 10:53:00 -0400

    drag dapp list

--- src/ui/app/pages/home/home/dapp/DappList.tsx
+++ src/ui/app/pages/home/home/dapp/DappList.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 
 import DappListItem from './DappListItem';
 import useConvertVerticalScrollToHorizontal from '_src/ui/app/hooks/useConvertVerticalScrollToHorizontal';
@@ -11,6 +11,7 @@ interface DappListProps {
 
 export const DappList: React.FC<DappListProps> = ({ dapps }) => {
     const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const mouseXRef = useRef<number | null>(null);
     useConvertVerticalScrollToHorizontal(scrollContainerRef);
 
     const listItems = useMemo(() => {
@@ -19,10 +20,32 @@ export const DappList: React.FC<DappListProps> = ({ dapps }) => {
         ));
     }, [dapps]);
 
+    const onMouseDown = useCallback((e: React.MouseEvent) => {
+        mouseXRef.current = e.nativeEvent.clientX;
+    }, []);
+
+    const onMouseMove = useCallback((e: React.MouseEvent) => {
+        if (mouseXRef.current === null) return;
+
+        scrollContainerRef.current?.scrollBy({
+            left: mouseXRef.current - e.nativeEvent.clientX,
+        });
+
+        mouseXRef.current = e.nativeEvent.clientX;
+    }, []);
+
+    const onMouseUp = useCallback(() => {
+        mouseXRef.current = null;
+    }, []);
+
     return (
         <div
             ref={scrollContainerRef}
-            className="flex overflow-x-auto no-scrollbar whitespace-nowrap py-2 bg-ethos-light-gray dark:bg-ethos-dark-background-secondary border-b border-ethos-light-purple dark:border-ethos-dark-background-default"
+            className="flex overflow-x-auto cursor-grab select-none no-scrollbar whitespace-nowrap py-2 bg-ethos-light-gray dark:bg-ethos-dark-background-secondary border-b border-ethos-light-purple dark:border-ethos-dark-background-default"
+            onMouseDown={onMouseDown}
+            onMouseMove={onMouseMove}
+            onMouseUp={onMouseUp}
+            onMouseLeave={onMouseUp}
         >
             {listItems}
         </div>
