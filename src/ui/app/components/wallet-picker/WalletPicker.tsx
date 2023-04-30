020e7a3fb806056bec3ec1da2ad63b5166daa4ff
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-30 16:52:54 -0400

    hide import wallet feature in prod

--- src/ui/app/components/wallet-picker/WalletPicker.tsx
+++ src/ui/app/components/wallet-picker/WalletPicker.tsx
@@ -7,6 +7,7 @@ import Button from '../../shared/buttons/Button';
 import Body from '../../shared/typography/Body';
 import WalletList from '../../shared/wallet-list/WalletList';
 import LoadingIndicator from '../loading/LoadingIndicator';
+import { useDependencies } from '_src/shared/utils/dependenciesContext';
 
 interface WalletPickerProps {
     selectOnly?: boolean;
@@ -17,6 +18,8 @@ const WalletPicker = ({
     selectOnly = false,
     isWalletEditing = false,
 }: WalletPickerProps) => {
+    const { featureFlags } = useDependencies();
+
     const [createWallet, setCreateWallet] = useState<() => void>(
         () => () => null
     );
@@ -54,13 +57,15 @@ const WalletPicker = ({
                                     )}
                                 </Button>
                             </CreateWalletProvider>
-                            <Button
-                                buttonStyle="secondary"
-                                to="/home/manage-wallets"
-                                disabled={loading}
-                            >
-                                Import / Manage External Wallets
-                            </Button>
+                            {featureFlags.showWipFeatures && (
+                                <Button
+                                    buttonStyle="secondary"
+                                    to="/home/manage-wallets"
+                                    disabled={loading}
+                                >
+                                    Import / Manage External Wallets
+                                </Button>
+                            )}
                         </div>
                     ) : (
                         <div className="flex gap-2 py-4 px-5 place-content-center">
