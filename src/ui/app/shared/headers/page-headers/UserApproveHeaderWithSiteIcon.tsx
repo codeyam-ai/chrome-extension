063a39dd3c4ff533c5ccc0d54bddd80a9516b87d
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-05-02 11:36:36 -0700

    shows fallback icon in user approve popup if dapp does not have favicon

--- src/ui/app/shared/headers/page-headers/UserApproveHeaderWithSiteIcon.tsx
+++ src/ui/app/shared/headers/page-headers/UserApproveHeaderWithSiteIcon.tsx
@@ -4,6 +4,7 @@ import HeaderWithIcons from './HeaderWithIcons';
 import EthosLogo from '_src/ui/app/components/logos/EthosLogo';
 
 import type { SyntheticEvent } from 'react';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
 
 interface UserApproveHeaderWithSiteIconProps {
     iconSrc?: string;
@@ -41,7 +42,7 @@ const UserApproveHeaderWithSiteIcon = ({
             firstIcon={
                 isConnectingToEthosDashboard ? (
                     <EthosLogo className="h-10 w-10" />
-                ) : (
+                ) : iconSrc ? (
                     <img
                         src={iconSrc}
                         className="w-10 aspect-square rounded-full hidden relative"
@@ -49,11 +50,15 @@ const UserApproveHeaderWithSiteIcon = ({
                         onError={hideIcon}
                         onLoad={showIcon}
                     />
+                ) : (
+                    <div className="rounded-full p-2 h-10 w-10 border-[0.5px] border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke bg-ethos-light-background-secondary dark:bg-ethos-dark-background-secondary">
+                        <GlobeAltIcon className="h-full w-full text-ethos-light-text-default dark:text-ethos-dark-text-default" />
+                    </div>
                 )
             }
             secondIcon={
                 isConnectingToEthosDashboard ? null : (
-                    <div className="rounded-full p-1 h-10 w-10 bg-ethos-light-background-secondary dark:bg-ethos-dark-background-secondary">
+                    <div className="rounded-full p-1 h-10 w-10 border-[0.5px] border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke bg-ethos-light-background-secondary dark:bg-ethos-dark-background-secondary">
                         <EthosLogo className="h-full w-full" />
                     </div>
                 )
