169923f64a86c6f5f49a76ceaf4c05e4a856679d
Kevin Jasker <kevinjasker@gmail.com>
2023-05-02 06:34:07 -0700

    fix up stake estimated rewards display logic

--- src/ui/app/pages/home/home/dapp/dapps/Staking/StakeDetail.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/StakeDetail.tsx
@@ -1,20 +1,27 @@
 import { MinusCircleIcon } from '@heroicons/react/24/outline';
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
 import {
     SUI_SYSTEM_STATE_OBJECT_ID,
     SUI_TYPE_ARG,
     TransactionBlock,
 } from '@mysten/sui.js';
 import { useQueryClient } from '@tanstack/react-query';
+import { formatRelative } from 'date-fns';
 import { useCallback, useMemo, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+import { NUM_OF_EPOCH_BEFORE_EARNING } from '_src/shared/constants';
+import ClickableTooltip from '_src/ui/app/components/ClickableTooltip';
 import { getSigner } from '_src/ui/app/helpers/getSigner';
+import getTimeToEarnStakeRewards from '_src/ui/app/helpers/staking/getTimeToEarnStakeRewards';
 import { useAppSelector, useFormatCoin } from '_src/ui/app/hooks';
 import useGetDelegatedStakes from '_src/ui/app/hooks/staking/useGetDelegatedStakes';
+import { useSystemState } from '_src/ui/app/hooks/staking/useSystemState';
 import { useValidatorsWithApy } from '_src/ui/app/hooks/staking/useValidatorsWithApy';
 import mistToSui from '_src/ui/app/pages/dapp-tx-approval/lib/mistToSui';
 import { FailAlert } from '_src/ui/app/shared/alerts/FailAlert';
+import Button from '_src/ui/app/shared/buttons/Button';
 import ConfirmDestructiveActionDialog from '_src/ui/app/shared/dialog/ConfirmDestructiveActionDialog';
 import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
@@ -106,6 +113,8 @@ const StakeDetail: React.FC = () => {
     );
 };
 
+const REMOVE_STAKE_INFO_TEXT =
+    'Rewards are not earned until you have Unstaked.';
 const StakeRow = ({ stake }: { stake: Stake }) => {
     const {
         activeAccountIndex,
@@ -129,6 +138,7 @@ const StakeRow = ({ stake }: { stake: Stake }) => {
         () => setIsModalOpen(true),
         [setIsModalOpen]
     );
+
     const onConfirmRevokeStake = useCallback(async () => {
         if (!stakedSuiId) return;
 
@@ -183,36 +193,72 @@ const StakeRow = ({ stake }: { stake: Stake }) => {
         setIsModalOpen(false);
     }, [setIsModalOpen]);
 
-    return (
-        <div className="w-full flex flex-row items-center place-content-center justify-between py-3 px-4 border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke">
-            <div className="flex flex-col items-start">
-                <Body isSemibold>
-                    {formattedPrincipal} {symbol}
-                </Body>
-                <Body isTextColorMedium>{stake.status}</Body>
-            </div>
+    const { data: systemState } = useSystemState();
 
-            <div className="flex flex-col items-start">
-                <div className="flex gap-1">
-                    <Body isTextColorMedium>Est. Reward:</Body>
+    const earningRewardsEpoch =
+        Number(stake?.stakeRequestEpoch) + NUM_OF_EPOCH_BEFORE_EARNING;
+    const isEarnedRewards =
+        Number(systemState?.epoch) >= Number(earningRewardsEpoch);
+
+    const formattedDistanceToRewards = useMemo(() => {
+        const timeToRewardsStart = getTimeToEarnStakeRewards(
+            earningRewardsEpoch,
+            systemState
+        );
+        return formatRelative(new Date(timeToRewardsStart), new Date());
+    }, [systemState, earningRewardsEpoch]);
+
+    const estimatedReward = isEarnedRewards
+        ? mistToSui(Number(stake.estimatedReward), 9)
+        : 0.0;
+
+    return (
+        <div className="w-full flex flex-col py-3 px-4 border-b border-ethos-light-text-stroke dark:border-ethos-dark-text-stroke">
+            <div className="flex justify-between items-center">
+                <div className="flex flex-col items-start">
                     <Body isSemibold>
-                        {mistToSui(Number(stake.estimatedReward), 9)} SUI
+                        {formattedPrincipal} {symbol}
                     </Body>
+                    <Body isTextColorMedium>{stake.status}</Body>
+                </div>
+
+                <div className="flex">
+                    <div className="flex gap-1">
+                        <Body isTextColorMedium>Earned Reward:</Body>
+                        <Body isSemibold>{estimatedReward} SUI</Body>
+                    </div>
                 </div>
             </div>
-
-            <button onClick={onClickRevokeStake} className="p-2">
-                <MinusCircleIcon className="h-5 w-5 text-ethos-light-primary-light dark:text-ethos-dark-primary-dark" />
-            </button>
+            <div className="flex w-full justify-end items-center">
+                {!isEarnedRewards && (
+                    <div className="pr-2">
+                        <Body>Starts earning {formattedDistanceToRewards}</Body>
+                    </div>
+                )}
+                <Button
+                    isInline
+                    removeContainerPadding
+                    buttonStyle="secondary"
+                    onClick={onClickRevokeStake}
+                >
+                    <ClickableTooltip
+                        message={REMOVE_STAKE_INFO_TEXT}
+                        tooltipPosition="below"
+                    >
+                        <InformationCircleIcon className="h-4 w-4 text-ethos-light-primary-light dark:text-ethos-dark-primary-dark" />
+                    </ClickableTooltip>
+                    Unstake
+                </Button>
+            </div>
             <ConfirmDestructiveActionDialog
                 primaryActionIsLoading={loading}
                 onCancel={onCancelConfirmRevokeStake}
                 onConfirm={onConfirmRevokeStake}
                 isOpen={isModalOpen}
                 setIsOpen={setIsModalOpen}
-                title="Are you sure you want to revoke this Stake?"
+                title="Are you sure you want to Unstake?"
                 description="This action cannot be undone."
-                primaryButtonText="Revoke"
+                primaryButtonText="Unstake SUI"
                 secondaryButtonText="Cancel"
             />
         </div>
