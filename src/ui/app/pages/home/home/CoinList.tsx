6e4af9c4f042361f8e58a3399519b3221c8b7d30
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-05-02 18:13:06 -0700

    change styles in coin list

--- src/ui/app/pages/home/home/CoinList.tsx
+++ src/ui/app/pages/home/home/CoinList.tsx
@@ -2,16 +2,15 @@ import CoinBalanceElement from './CoinBalance';
 
 const CoinList = ({ balances }: { balances: Record<string, bigint> }) => {
     return (
-        <div className="text-left" data-testid="coin-list">
+        <div className="text-left space-y-2" data-testid="coin-list">
             {Object.keys(balances).map((type: string) => {
                 const balance = balances[type];
                 return (
-                    <div className={'mb-5'} key={type}>
-                        <CoinBalanceElement
-                            type={type}
-                            balance={balance.toString()}
-                        />
-                    </div>
+                    <CoinBalanceElement
+                        key={type}
+                        type={type}
+                        balance={balance.toString()}
+                    />
                 );
             })}
         </div>
