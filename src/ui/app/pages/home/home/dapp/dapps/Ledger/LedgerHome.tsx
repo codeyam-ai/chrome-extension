fe6c96b0c5360f4e33b36d9991571a5ef5f63702
Jared Cosulich <jared.cosulich@gmail.com>
2023-05-06 21:21:13 -0400

    ledger can not connect error

--- src/ui/app/pages/home/home/dapp/dapps/Ledger/LedgerHome.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Ledger/LedgerHome.tsx
@@ -80,6 +80,7 @@ const LedgerHome = () => {
     const readableError = useMemo(() => {
         if (!connectionError) return;
 
+        console.log('connectionError', connectionError);
         if (
             connectionError ===
             "Error: The user doesn't have a Ledger device connected to their machine"
@@ -88,6 +89,15 @@ const LedgerHome = () => {
                 title: 'Not Connected',
                 message:
                     'Please connect your ledger to your computer, enter your passcode, and launch the Sui app.',
+            };
+        } else if (
+            connectionError ===
+            'TransportStatusError: Ledger device: INS_NOT_SUPPORTED (0x6d00)'
+        ) {
+            return {
+                title: 'Ledger Error',
+                message:
+                    'The connection with Ledger does not yet support your device. It is in active development and should improve soon.',
             };
         }
     }, [connectionError]);
