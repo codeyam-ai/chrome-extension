2719685ddf61b30f67929967a49826120098e1c5
Kevin Jasker <kevinjasker@gmail.com>
2023-05-09 13:31:55 -0700

    make entirety of StakedInfo clickable

--- src/ui/app/pages/home/home/StakedInfo.tsx
+++ src/ui/app/pages/home/home/StakedInfo.tsx
@@ -1,11 +1,11 @@
+import { SUI_TYPE_ARG } from '@mysten/sui.js';
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 import StakingLogo from '_images/dapps/logos/staking.png';
 import { useFormatCoin } from '_src/ui/app/hooks';
 import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
-import EthosLink from '_src/ui/app/shared/typography/EthosLink';
-import { SUI_TYPE_ARG } from '@mysten/sui.js';
 
 interface StakedInfoProps {
     totalActivePendingStakedSUI: bigint;
@@ -20,28 +20,29 @@ const StakedInfo: React.FC<StakedInfoProps> = ({
 
     return (
         <div data-testid="staked-sui-info">
-            <Body className="ml-1">Currently Staked SUI</Body>
-            <div className="flex w-full mb-4 rounded-lg bg-ethos-light-background-light-grey dark:bg-ethos-dark-background-light-grey">
-                <img
-                    src={StakingLogo}
-                    alt="staking logo"
-                    className="object-cover w-[60px] h-[60px] rounded-l-lg"
-                />
-                <div className="flex w-full justify-between items-center px-4">
-                    <BodyLarge isSemibold>
-                        {formattedStakedSui} {symbol}
-                    </BodyLarge>
-                    <EthosLink
-                        type="internal"
-                        to="/home/staking"
-                        className="text-sm"
-                    >
-                        {totalActivePendingStakedSUI
-                            ? 'Go to Staking'
-                            : 'Stake'}
-                    </EthosLink>
+            <Body className="ml-1 mb-2">Currently Staked SUI</Body>
+            <Link to="/home/staking">
+                <div className="flex w-full mb-4 rounded-lg bg-ethos-light-background-light-grey dark:bg-ethos-dark-background-light-grey">
+                    <img
+                        src={StakingLogo}
+                        alt="staking logo"
+                        className="object-cover w-[60px] h-[60px] rounded-l-lg"
+                    />
+                    <div className="flex w-full justify-between items-center px-4">
+                        <BodyLarge isSemibold>
+                            {formattedStakedSui} {symbol}
+                        </BodyLarge>
+                        <Body
+                            isSemibold
+                            className="text-ethos-light-primary-light dark:text-ethos-dark-primary-dark"
+                        >
+                            {totalActivePendingStakedSUI
+                                ? 'Go to Staking'
+                                : 'Stake'}
+                        </Body>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
