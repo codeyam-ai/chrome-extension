a8241ecb2048094e4ae5af8a39c62bdea601e72e
Kevin Jasker <kevinjasker@gmail.com>
2023-05-10 13:47:35 -0700

    add ValidatorList sort component

--- src/ui/app/pages/home/home/dapp/dapps/Staking/ValidatorList.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Staking/ValidatorList.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import Loading from '_src/ui/app/components/loading';
 import truncateMiddle from '_src/ui/app/helpers/truncate-middle';
 import { useValidatorsWithApy } from '_src/ui/app/hooks/staking/useValidatorsWithApy';
+import Radio from '_src/ui/app/shared/inputs/Radio';
+import ArrowUpDownSort from '_src/ui/app/shared/svg/ArrowUpDownSort';
 import Body from '_src/ui/app/shared/typography/Body';
 
 import type { SuiAddress, SuiValidatorSummary } from '@mysten/sui.js';
@@ -11,6 +13,7 @@ import type { SuiAddress, SuiValidatorSummary } from '@mysten/sui.js';
 export interface SuiValidatorSummaryWithApy extends SuiValidatorSummary {
     apy: number;
     isApyApproxZero: boolean;
+    stakeShare: number;
 }
 
 interface ValidatorListProps {
@@ -18,20 +21,98 @@ interface ValidatorListProps {
     selectedValidator?: SuiAddress;
 }
 
+export type SortDirection = 'asc' | 'desc';
+type SortKey = 'apy' | 'stakeShare' | 'name';
+
+type ValidatorSortFunction = (
+    SortDirection: SortDirection,
+    a: SuiValidatorSummaryWithApy,
+    b: SuiValidatorSummaryWithApy
+) => number;
+
+type ValidatorSortFunctions = {
+    [sortKey in SortKey]: ValidatorSortFunction;
+};
+
+const validatorSortFunctions: ValidatorSortFunctions = {
+    apy: (sortDirection, a, b) => {
+        return sortDirection === 'desc' ? b.apy - a.apy : a.apy - b.apy;
+    },
+    stakeShare: (sortDirection, a, b) => {
+        return sortDirection === 'desc'
+            ? b.stakeShare - a.stakeShare
+            : a.stakeShare - b.stakeShare;
+    },
+    name: (sortDirection, a, b) => {
+        return sortDirection === 'desc'
+            ? b.name.localeCompare(a.name)
+            : a.name.localeCompare(b.name);
+    },
+};
 const ValidatorList: React.FC<ValidatorListProps> = ({
     onSelectValidator,
     selectedValidator,
 }) => {
+    const [sortKey, setSortKey] = useState<SortKey>('apy');
+    const [sortDirection, setSortDirection] = useState<SortDirection>('desc');
+
     const { isFetching, data: validators } = useValidatorsWithApy();
 
     const sortedValidators = useMemo(() => {
-        if (!validators) return;
-        return Object.values(validators).sort((a, b) => b.apy - a.apy);
-    }, [validators]);
+        if (!validators || !sortKey || !sortDirection) return;
+        const validatorValues = Object.values(validators);
+
+        return validatorValues.sort((a, b) =>
+            validatorSortFunctions[sortKey](sortDirection, a, b)
+        );
+    }, [validators, sortKey, sortDirection]);
+
+    const onSortKeyChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            if (event.target.name === sortKey) return;
+
+            setSortKey(event.target.name as SortKey);
+            setSortDirection('desc');
+        },
+        [sortKey]
+    );
+
+    const onSortDirectionChange = useCallback(() => {
+        if (sortDirection === 'asc') setSortDirection('desc');
+        if (sortDirection === 'desc') setSortDirection('asc');
+    }, [sortDirection, setSortDirection]);
 
     return (
         <Loading loading={isFetching} big={true}>
             <div className="flex flex-col">
+                <div className="flex w-full p-3.5 rounded-lg items-center bg-ethos-light-background-light-grey dark:bg-ethos-dark-background-light-grey">
+                    <button
+                        onClick={onSortDirectionChange}
+                        className="flex pr-3"
+                    >
+                        <ArrowUpDownSort sortDirection={sortDirection} />
+                    </button>
+                    <fieldset className="flex justify-between w-full">
+                        <Radio
+                            label="Stake Share"
+                            id="stakeShare"
+                            onChange={onSortKeyChange}
+                            checked={sortKey === 'stakeShare'}
+                        />
+                        <Radio
+                            label="APY"
+                            id="apy"
+                            onChange={onSortKeyChange}
+                            checked={sortKey === 'apy'}
+                        />
+                        <Radio
+                            label="Name"
+                            id="name"
+                            onChange={onSortKeyChange}
+                            checked={sortKey === 'name'}
+                        />
+                    </fieldset>
+                </div>
                 {!isFetching &&
                     sortedValidators &&
                     sortedValidators.map((validator, key) => (
@@ -102,4 +183,5 @@ const ValidatorRow: React.FC<ValidatorRowProps> = ({
         </button>
     );
 };
+
 export default ValidatorList;
