4a2c164e8fef414cc2e22c8ca97a9d758e1af4df
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-05-17 15:21:11 -0700

    add radio buttons to change pfp selection type

--- src/ui/app/pages/home/home/dapp/dapps/Customize/ChangeProfilePicture/ChangeProfilePicturePage.tsx
+++ src/ui/app/pages/home/home/dapp/dapps/Customize/ChangeProfilePicture/ChangeProfilePicturePage.tsx
@@ -1,13 +1,38 @@
+import { useCallback, useState } from 'react';
+
 import ChangeEmojiAndColor from './ChangeEmojiAndColor';
+import Radio from '_src/ui/app/shared/inputs/Radio';
 import Title from '_src/ui/app/shared/typography/Title';
 
 const ChangeProfilePicture: React.FC = () => {
+    const [pfpType, setPfpType] = useState<'emoji' | 'nft'>('emoji');
+
+    const onRadioChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            setPfpType(event.target.name as 'emoji' | 'nft');
+        },
+        []
+    );
+
     return (
         <div className="flex flex-col items-center pt-6 px-6">
-            <Title className="pb-6">
-                Choose your wallet&apos;s profile picture
-            </Title>
-            <ChangeEmojiAndColor />
+            <Title>Choose your wallet&apos;s profile picture</Title>
+            <div className="flex gap-4 py-4">
+                <Radio
+                    label="Emoji"
+                    id="emoji"
+                    onChange={onRadioChange}
+                    checked={pfpType === 'emoji'}
+                />
+                <Radio
+                    label="NFT"
+                    id="nft"
+                    onChange={onRadioChange}
+                    checked={pfpType === 'nft'}
+                />
+            </div>
+            <hr className="w-full text-ethos-light-text-stroke dark:text-ethos-dark-text-stroke pb-4" />
+            {pfpType === 'emoji' ? <ChangeEmojiAndColor /> : <div>NFT</div>}
         </div>
     );
 };
