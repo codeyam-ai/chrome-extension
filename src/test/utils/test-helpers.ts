6375aee93f0f4b4e310a2263c39aff4e434c77b4
Austin Gatlin <austin@gatlin.io>
2023-06-07 15:50:55 +1000

    test: add initial test coverage for multiple NFTs

--- src/test/utils/test-helpers.ts
+++ src/test/utils/test-helpers.ts
@@ -6,3 +6,25 @@ export const preventActWarning = function () {
     // also perhaps a fix is underway here: https://github.com/testing-library/react-testing-library/pull/1137
     cleanup();
 };
+
+/**
+ * Return any number of different objectIds, for usage in the mockchain.
+ *
+ * @param count The number of different objectIds you want.
+ */
+export function getObjectIds(count: number): string[] {
+    if (count <= 0)
+        throw new Error(`objectIds count less than 1 not supported: ${count}`);
+    if (count >= 10)
+        throw new Error(
+            `objectIds count greater than 9 not supported: ${count}`
+        );
+
+    const objectIds = [];
+    for (let i = 0; i < count; i++) {
+        objectIds.push(
+            `0x3c36fe1eca57222e087352959ab0edf83251fe0a5aa8a0ec87c4e3fa1714f36${i}`
+        );
+    }
+    return objectIds;
+}
