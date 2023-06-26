f0e7b5ad9ec0cf53cc67801a1d0f6948999ce00a
Adam Wade <adamwade2384@gmail.com>
2023-06-26 12:48:23 -0700

    Add affiliate / destination address for heroswap

--- src/ui/app/pages/home/buy/providers/heroswap.tsx
+++ src/ui/app/pages/home/buy/providers/heroswap.tsx
@@ -4,11 +4,13 @@ import { useTheme } from '_src/shared/utils/themeContext';
 
 export default function HeroswapOnboarding() {
     const [selectedApiEnv] = useAppSelector(({ app }) => [app.apiEnv]);
+    const { address } = useAppSelector(({ account }) => account);
     const { resolvedTheme } = useTheme();
 
     const isProduction = selectedApiEnv.toString() === API_ENV.mainNet;
     const network = isProduction ? 'mainnet' : 'testnet';
     const theme = resolvedTheme === 'dark' ? 'dark-purple' : 'light-purple';
+    const affiliateAddress = '0x10742A09435949F4bA49460E9EA689cA9cBa4550';
 
     return (
         <div
@@ -17,7 +19,7 @@ export default function HeroswapOnboarding() {
         >
             <iframe
                 id={'heroswap-iframe'}
-                src={`https://heroswap.com/widget?theme=${theme}&network=${network}&destinationTicker=SUI`}
+                src={`https://heroswap.com/widget?theme=${theme}&network=${network}&destinationTicker=SUI&affiliateAddress=${affiliateAddress}&destinationAddress=${address}`}
                 className={'border-0 m-0 p-0 w-full h-full relative z-20'}
             />
         </div>
