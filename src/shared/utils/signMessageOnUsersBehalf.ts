6a7a72f32fea9e2d78a17679b2961d65433b18db
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-07-03 18:10:18 -0700

    can get JWT

---
+++ src/shared/utils/signMessageOnUsersBehalf.ts
@@ -0,0 +1,51 @@
+import {
+    fromB64,
+    toB64,
+    type SignedMessage,
+    type SuiTransactionBlockResponse,
+} from '@mysten/sui.js';
+
+import { thunkExtras } from '_redux/store/thunk-extras';
+import { getSigner } from '_src/ui/app/helpers/getSigner';
+
+import type SuiLedgerClient from '@mysten/ledgerjs-hw-app-sui';
+import type { AccountInfo } from '_src/ui/app/KeypairVault';
+
+const signMessageOnUsersBehalf = async (
+    connectToLedger: () => Promise<SuiLedgerClient>,
+    message: string,
+    passphrase: string | null,
+    authentication: string | null,
+    address: string | null,
+    accountInfos: AccountInfo[],
+    activeAccountIndex: number
+) => {
+    let txResult: SuiTransactionBlockResponse | SignedMessage | undefined =
+        undefined;
+    let txResultError: string | undefined;
+
+    const signer = await getSigner(
+        passphrase,
+        accountInfos,
+        address,
+        authentication,
+        activeAccountIndex,
+        connectToLedger
+    );
+
+    if (!signer) {
+        throw new Error(`Signer not found`);
+    }
+
+    try {
+        txResult = await signer.signMessage({
+            message: fromB64(message),
+        });
+    } catch (e) {
+        txResultError = (e as Error).message;
+    }
+
+    return txResult;
+};
+
+export default signMessageOnUsersBehalf;
--- src/ui/app/pages/home/home/TESTServerCustomizations.tsx
+++ src/ui/app/pages/home/home/TESTServerCustomizations.tsx
@@ -1,12 +1,70 @@
+import { fromB64 } from '@mysten/bcs';
+import signMessageOnUsersBehalf from '_src/shared/utils/signMessageOnUsersBehalf';
 import {
     authApiCall,
     explorerApiCall,
     simpleApiCall,
 } from '_src/shared/utils/simpleApiCall';
+import { useSuiLedgerClient } from '_src/ui/app/components/ledger/SuiLedgerClientProvider';
+import { useAppSelector } from '_src/ui/app/hooks';
 import Button from '_src/ui/app/shared/buttons/Button';
 import { useCallback } from 'react';
 
 const TestServerCustomizations: React.FC = () => {
+    const { connectToLedger } = useSuiLedgerClient();
+    const {
+        activeAccountIndex,
+        accountInfos,
+        address: activeAddress,
+        authentication,
+        passphrase,
+    } = useAppSelector(({ account }) => account);
+
+    const sign = useCallback(async () => {
+        const dataToSign = {
+            tenantId: '92dd81a5-dd8b-480c-b468-66f69a74c1bc',
+            timestamp: Date.now(),
+        };
+
+        // const dataToSignBase64 = fromB64(JSON.stringify(dataToSign));
+        const dataToSignBase64 = Buffer.from(
+            JSON.stringify(dataToSign)
+        ).toString('base64');
+
+        const txResult = await signMessageOnUsersBehalf(
+            connectToLedger,
+            dataToSignBase64,
+            passphrase,
+            authentication,
+            activeAddress,
+            accountInfos,
+            activeAccountIndex
+        );
+
+        const authReqBody: Record<string, string> = {
+            b64Message: dataToSignBase64,
+            b64Signature: txResult?.signature ?? '',
+        };
+
+        const res = await authApiCall({
+            relativePath: 'v1/auth/sui/signature',
+            method: 'POST',
+            accessToken: '',
+            body: authReqBody,
+        });
+
+        const { jwt } = res.json;
+
+        console.log('jwt :>> ', jwt);
+    }, [
+        accountInfos,
+        activeAccountIndex,
+        activeAddress,
+        authentication,
+        connectToLedger,
+        passphrase,
+    ]);
+
     const testSimple = useCallback(async () => {
         // const res = await explorerApiCall({
         //     relativePath: 'v1/user/profile',
@@ -24,7 +82,8 @@ const TestServerCustomizations: React.FC = () => {
     return (
         <div>
             <h1>Test Server Customizations</h1>
-            <Button onClick={testSimple}>Test</Button>
+            <Button onClick={sign}>Sign</Button>
+            <Button onClick={testSimple}>Auth API Call</Button>
         </div>
     );
 };
