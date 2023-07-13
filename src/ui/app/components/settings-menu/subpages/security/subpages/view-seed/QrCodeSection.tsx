4500cbeb3e309e193555f26af2bd733507a05240
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-07-13 00:01:28 -0700

    add info dialog explaining personalization sync

--- src/ui/app/components/settings-menu/subpages/security/subpages/view-seed/QrCodeSection.tsx
+++ src/ui/app/components/settings-menu/subpages/security/subpages/view-seed/QrCodeSection.tsx
@@ -1,6 +1,16 @@
-import { useEffect, useCallback, useState, useRef } from 'react';
-import MnemonicQrCode from './MnemonicQrCode';
+import { useAppDispatch } from '_src/ui/app/hooks';
+import {
+    loadCustomizationsSyncPreference,
+    saveCustomizationsSyncPreference,
+} from '_src/ui/app/redux/slices/account';
 import ExpandableSection from '_src/ui/app/shared/content/ExpandableSection';
+import Toggle from '_src/ui/app/shared/inputs/Toggle';
+import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
+import { useCallback, useEffect, useState } from 'react';
+import MnemonicQrCode from './MnemonicQrCode';
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
+import InfoDialog from '_src/ui/app/shared/dialog/InfoDialog';
+import { SparklesIcon } from '@heroicons/react/24/solid';
 
 interface QrCodeSectionProps {
     mnemonic: string;
@@ -9,13 +19,40 @@ interface QrCodeSectionProps {
 const easeOutQuad = (t: number) => t * (2 - t);
 
 const QrCodeSection: React.FC<QrCodeSectionProps> = ({ mnemonic }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-    const scrollAnimationRef = useRef<number | null>(null);
+    const [isExpandOpen, setIsExpandOpen] = useState(false);
+    const [isSyncEnabled, setIsSyncEnabled] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const dispatch = useAppDispatch();
 
-    const handleToggle = useCallback(() => {
-        setIsOpen(!isOpen);
-    }, [isOpen]);
+    const handleToggleSync = useCallback(
+        async (value: boolean) => {
+            setIsSyncEnabled(value);
+            await dispatch(saveCustomizationsSyncPreference(value));
+        },
+        [dispatch]
+    );
+
+    const handleToggleExpand = useCallback(() => {
+        setIsExpandOpen(!isExpandOpen);
+    }, [isExpandOpen]);
+
+    const openModal = useCallback(() => {
+        setIsModalOpen(true);
+    }, []);
+
+    const closeModal = useCallback(() => {
+        setIsModalOpen(false);
+    }, []);
+
+    useEffect(() => {
+        const getCustomizationSyncPreference = async () => {
+            const { payload: preference } = await dispatch(
+                loadCustomizationsSyncPreference()
+            );
+            setIsSyncEnabled(preference as boolean);
+        };
+        getCustomizationSyncPreference();
+    }, [dispatch]);
 
     // This is the easing function
     function easeInOutCubic(t: number) {
@@ -25,7 +62,7 @@ const QrCodeSection: React.FC<QrCodeSectionProps> = ({ mnemonic }) => {
     }
 
     useEffect(() => {
-        if (isOpen) {
+        if (isExpandOpen) {
             const start = performance.now();
             const element = document.getElementById('settings-container');
             if (!element) return;
@@ -48,17 +85,41 @@ const QrCodeSection: React.FC<QrCodeSectionProps> = ({ mnemonic }) => {
             };
             requestAnimationFrame(smoothScroll);
         }
-    }, [isOpen]);
+    }, [isExpandOpen]);
 
     return (
         <div>
             <ExpandableSection
-                isOpen={isOpen}
-                onToggle={handleToggle}
+                isOpen={isExpandOpen}
+                onToggle={handleToggleExpand}
                 title="Import Wallet in Mobile App"
             >
                 <MnemonicQrCode mnemonic={mnemonic} />
+                <div className="flex flex-row justify-between items-center mt-4">
+                    <div className="flex flex-row items-center">
+                        <BodyLarge className="text-left">
+                            Sync Personalization
+                        </BodyLarge>
+                        <div className="cursor-pointer p-1" onClick={openModal}>
+                            <QuestionMarkCircleIcon className="w-5 h-5 text-ethos-light-text-medium dark:text-ethos-dark-text-medium" />
+                        </div>
+                    </div>
+                    <Toggle
+                        isChecked={isSyncEnabled}
+                        onToggle={handleToggleSync}
+                    />
+                </div>
             </ExpandableSection>
+            <InfoDialog
+                isOpen={isModalOpen}
+                setIsOpen={setIsModalOpen}
+                title="Personalization Sync"
+                paragraphs={[
+                    "Choose if you want your wallets' personalizations such as nickname, color, and profile picture to be synced across your devices.",
+                    'This data is fully encrypted on Ethos servers and can only be decrypted with your private key.',
+                ]}
+                iconWithNoClasses={<SparklesIcon />}
+            />
         </div>
     );
 };
