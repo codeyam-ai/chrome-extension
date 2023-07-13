92f4c4292514c426914682968a09dc4d11253e7b
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-07-13 01:23:00 -0700

    break out SyncCustomizationsToggle and improve sync between extensions

--- src/ui/app/components/settings-menu/subpages/security/subpages/PersonalizationSync.tsx
+++ src/ui/app/components/settings-menu/subpages/security/subpages/PersonalizationSync.tsx
@@ -16,51 +16,9 @@ import Toggle from '_src/ui/app/shared/inputs/Toggle';
 import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
 import Header from '_src/ui/app/shared/typography/Header';
+import SyncCustomizationsToggle from '_src/shared/utils/customizationsSync/SyncCustomizationsToggle';
 
 const PersonalizationSync: React.FC = () => {
-    const [isEnabled, setIsEnabled] = useState(false);
-    const [loadingText, setLoadingText] = useState<string>();
-    const dispatch = useAppDispatch();
-    const { resolvedTheme } = useTheme();
-    const { accountInfos, mnemonic } = useAppSelector(({ account }) => account);
-    const provider = api.instance.fullNode;
-
-    const handleToggle = useCallback(
-        async (value: boolean) => {
-            setIsEnabled(value);
-            await dispatch(saveCustomizationsSyncPreference(value));
-            if (value) {
-                setLoadingText('Syncing personalization');
-                await saveAllCustomizationsFromSeed(
-                    mnemonic ?? '',
-                    accountInfos,
-                    provider
-                );
-                toast(<SuccessAlert text={'Personalization synced'} />);
-            } else {
-                setLoadingText('Deleting synced data from server');
-                await deleteAllCustomizationsFromSeed(
-                    mnemonic ?? '',
-                    accountInfos,
-                    provider
-                );
-                toast(<SuccessAlert text={'Synced data removed'} />);
-            }
-            setLoadingText(undefined);
-        },
-        [accountInfos, dispatch, mnemonic, provider]
-    );
-
-    useEffect(() => {
-        const getCustomizationSyncPreference = async () => {
-            const { payload: preference } = await dispatch(
-                loadCustomizationsSyncPreference()
-            );
-            setIsEnabled(preference as boolean);
-        };
-        getCustomizationSyncPreference();
-    }, [dispatch]);
-
     return (
         <div className="p-6 flex flex-col gap-6">
             <Header className="text-left">Personalization Sync</Header>
@@ -76,27 +34,7 @@ const PersonalizationSync: React.FC = () => {
                 decrypted with your private key.
             </BodyLarge>
 
-            <div className="flex flex-row justify-between items-center">
-                <BodyLarge className="text-left">
-                    Sync Personalization
-                </BodyLarge>
-                <Toggle isChecked={isEnabled} onToggle={handleToggle} />
-            </div>
-
-            {loadingText && (
-                <div className="flex gap-4 items-center place-content-center">
-                    <PuffLoader
-                        color={
-                            resolvedTheme === 'light' ? '#6D28D9' : '#9C78F7'
-                        }
-                        loading={true}
-                        size={24}
-                        aria-label="Loading Spinner"
-                        data-testid="loader"
-                    />
-                    <Body>{loadingText}</Body>
-                </div>
-            )}
+            <SyncCustomizationsToggle />
         </div>
     );
 };
--- src/ui/app/components/settings-menu/subpages/security/subpages/view-seed/QrCodeSection.tsx
+++ src/ui/app/components/settings-menu/subpages/security/subpages/view-seed/QrCodeSection.tsx
@@ -5,13 +5,18 @@ import { PuffLoader } from 'react-spinners';
 import { toast } from 'react-toastify';
 
 import MnemonicQrCode from './MnemonicQrCode';
+import Authentication from '_src/background/Authentication';
 import { deleteAllCustomizationsFromSeed } from '_src/shared/utils/customizationsSync/deleteAllCustomizationsFromSeed';
+import { getAllCustomizationsFromSeed } from '_src/shared/utils/customizationsSync/getAllCustomizationsFromSeed';
 import { saveAllCustomizationsFromSeed } from '_src/shared/utils/customizationsSync/saveAllCustomizationsFromSeed';
 import { useTheme } from '_src/shared/utils/themeContext';
+import { type AccountInfo } from '_src/ui/app/KeypairVault';
 import { useAppDispatch, useAppSelector } from '_src/ui/app/hooks';
 import {
     loadCustomizationsSyncPreference,
+    saveAccountInfos,
     saveCustomizationsSyncPreference,
+    setAccountInfos,
 } from '_src/ui/app/redux/slices/account';
 import { api } from '_src/ui/app/redux/store/thunk-extras';
 import { SuccessAlert } from '_src/ui/app/shared/alerts/SuccessAlert';
@@ -20,6 +25,7 @@ import InfoDialog from '_src/ui/app/shared/dialog/InfoDialog';
 import Toggle from '_src/ui/app/shared/inputs/Toggle';
 import Body from '_src/ui/app/shared/typography/Body';
 import BodyLarge from '_src/ui/app/shared/typography/BodyLarge';
+import SyncCustomizationsToggle from '_src/shared/utils/customizationsSync/SyncCustomizationsToggle';
 
 interface QrCodeSectionProps {
     mnemonic: string;
@@ -27,57 +33,11 @@ interface QrCodeSectionProps {
 
 const QrCodeSection: React.FC<QrCodeSectionProps> = ({ mnemonic }) => {
     const [isExpandOpen, setIsExpandOpen] = useState(false);
-    const [isSyncEnabled, setIsSyncEnabled] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [loadingText, setLoadingText] = useState<string>();
-    const { resolvedTheme } = useTheme();
-    const { accountInfos } = useAppSelector(({ account }) => account);
-    const provider = api.instance.fullNode;
-    const dispatch = useAppDispatch();
-
-    const handleToggleSync = useCallback(
-        async (value: boolean) => {
-            setIsSyncEnabled(value);
-            await dispatch(saveCustomizationsSyncPreference(value));
-            if (value) {
-                setLoadingText('Syncing personalization');
-                await saveAllCustomizationsFromSeed(
-                    mnemonic ?? '',
-                    accountInfos,
-                    provider
-                );
-                toast(<SuccessAlert text={'Personalization synced'} />);
-            } else {
-                setLoadingText('Deleting synced data from server');
-                await deleteAllCustomizationsFromSeed(
-                    mnemonic ?? '',
-                    accountInfos,
-                    provider
-                );
-                toast(<SuccessAlert text={'Synced data removed'} />);
-            }
-            setLoadingText(undefined);
-        },
-        [accountInfos, dispatch, mnemonic, provider]
-    );
 
     const handleToggleExpand = useCallback(() => {
         setIsExpandOpen(!isExpandOpen);
     }, [isExpandOpen]);
-
-    const openModal = useCallback(() => {
-        setIsModalOpen(true);
-    }, []);
-
-    useEffect(() => {
-        const getCustomizationSyncPreference = async () => {
-            const { payload: preference } = await dispatch(
-                loadCustomizationsSyncPreference()
-            );
-            setIsSyncEnabled(preference as boolean);
-        };
-        getCustomizationSyncPreference();
-    }, [dispatch]);
 
     // This is the easing function
     function easeInOutCubic(t: number) {
@@ -120,39 +80,9 @@ const QrCodeSection: React.FC<QrCodeSectionProps> = ({ mnemonic }) => {
                 title="Import Wallet in Mobile App"
             >
                 <MnemonicQrCode mnemonic={mnemonic} />
-                <div className="flex flex-row justify-between items-center mt-4">
-                    <div className="flex flex-row items-center">
-                        <BodyLarge className="text-left">
-                            Sync Personalization
-                        </BodyLarge>
-                        <div className="cursor-pointer p-1" onClick={openModal}>
-                            <QuestionMarkCircleIcon className="w-5 h-5 text-ethos-light-text-medium dark:text-ethos-dark-text-medium" />
-                        </div>
-                    </div>
-                    <Toggle
-                        isChecked={isSyncEnabled}
-                        onToggle={handleToggleSync}
-                    />
+                <div className="mt-4">
+                    <SyncCustomizationsToggle showModalButton />
                 </div>
-                {loadingText ? (
-                    <div className="flex gap-4 items-center place-content-center mt-4">
-                        <PuffLoader
-                            color={
-                                resolvedTheme === 'light'
-                                    ? '#6D28D9'
-                                    : '#9C78F7'
-                            }
-                            loading={true}
-                            size={24}
-                            aria-label="Loading Spinner"
-                            data-testid="loader"
-                        />
-                        <Body>{loadingText}</Body>
-                    </div>
-                ) : (
-                    // Space so the content below doesn't jump
-                    <div className="h-[40px]" />
-                )}
             </ExpandableSection>
             <InfoDialog
                 isOpen={isModalOpen}
