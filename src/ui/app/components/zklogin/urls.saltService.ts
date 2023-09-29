8e2b7484e2089d156e4bb2e02da34823dcdde69b
Jared Cosulich <jared.cosulich@gmail.com>
2023-09-29 14:40:13 -0400

    revert salt service url

--- src/ui/app/components/zklogin/urls.saltService.ts
+++ src/ui/app/components/zklogin/urls.saltService.ts
@@ -2,7 +2,7 @@ type EnvType = typeof process.env.NODE_ENV | 'test';
 
 const ETHOS_SALT_SERVICE_DEV_URL =
     'https://salt-service-dev-a49a2100f792.herokuapp.com';
-// const ETHOS_SALT_SERVICE_LOCAL_URL = 'http://localhost:3005';
+const ETHOS_SALT_SERVICE_LOCAL_URL = 'http://localhost:3005';
 
 export function getSaltServiceUrl(
     { env }: { env: EnvType } = { env: process.env.NODE_ENV }
@@ -13,6 +13,6 @@ export function getSaltServiceUrl(
         case 'development':
         case 'test':
         default:
-            return ETHOS_SALT_SERVICE_DEV_URL; //ETHOS_SALT_SERVICE_LOCAL_URL;
+            return ETHOS_SALT_SERVICE_LOCAL_URL;
     }
 }
