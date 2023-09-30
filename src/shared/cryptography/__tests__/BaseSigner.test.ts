4c23f83c3843d49fe59fffff4399164492154eb8
Jared Cosulich <jared.cosulich@gmail.com>
2023-09-30 06:56:17 -0400

    BaseSigner test

--- src/shared/cryptography/__tests__/BaseSigner.test.ts
+++ src/shared/cryptography/__tests__/BaseSigner.test.ts
@@ -1,14 +1,13 @@
 import { SuiClient } from '@mysten/sui.js/client';
-import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
-import { blake2b } from '@noble/hashes/blake2b';
+import { Ed25519Keypair, type Ed25519PublicKey } from '@mysten/sui.js/keypairs/ed25519';
+import * as nobleHashes from '@noble/hashes/blake2b';
 
 import { BaseSigner } from '../BaseSigner';
 
-jest.mock('@noble/hashes/blake2b');
-
 describe('BaseSigner', () => {
+    const blake2bSpy = jest.spyOn(nobleHashes, 'blake2b');
     const keypair = new Ed25519Keypair();
-    const client = new SuiClient();
+    const client = new SuiClient({ url: 'http://localhost:3000' });
     const signer = new BaseSigner(keypair, client);
     const data = new Uint8Array([1, 2, 3]);
 
@@ -17,20 +16,28 @@ describe('BaseSigner', () => {
     });
 
     it('getAddress should return key pair public key in Sui address format', async () => {
-        const getAddressSpy = jest.spyOn(keypair, 'getPublicKey').mockReturnValue({toSuiAddress: () => 'base58check-address'});
+        const getAddressSpy = jest.spyOn(keypair, 'getPublicKey').mockReturnValue(
+            {toSuiAddress: () => 'sui-address'} as Ed25519PublicKey
+        );
         const result = await signer.getAddress();
-        expect(result).toEqual('base58check-address');
+        expect(result).toEqual('sui-address');
         expect(getAddressSpy).toHaveBeenCalled();
     });
 
     it('signData should sign data using key pair and return serialized signature', async () => {
-        const signDataSpy = jest.spyOn(keypair, 'signData').mockReturnValue(Promise.resolve(new Uint8Array([1,2,3,4])));
-        const publicKeySpy = jest.spyOn(keypair, 'getPublicKey').mockReturnValue(new Uint8Array([4,5,6,7]));
-        const blake2bSpy = blake2b as jest.Mock;
+        const signDataSpy = jest.spyOn(keypair, 'signData').mockReturnValue(
+            new Uint8Array([1,2,3,4])
+        );
+        const publicKeySpy = jest.spyOn(keypair, 'getPublicKey').mockReturnValue(
+            {
+                toSuiAddress: () => 'sui-address',
+                toBytes: () => new Uint8Array([4,5,6,7])
+            }  as unknown as Ed25519PublicKey
+        );
         blake2bSpy.mockReturnValue(new Uint8Array([8,9,10,11]));
 
         const result = await signer.signData(data);
-        expect(result).toEqual({signature: new Uint8Array([1,2,3,4]), signatureScheme: 'ED25519', publicKey: new Uint8Array([4,5,6,7])});
+        expect(result).toEqual('AAECAwQEBQYH');
         expect(signDataSpy).toHaveBeenCalledWith(new Uint8Array([8,9,10,11]));
         expect(publicKeySpy).toHaveBeenCalled();
         expect(blake2bSpy).toHaveBeenCalledWith(data, {dkLen: 32});
