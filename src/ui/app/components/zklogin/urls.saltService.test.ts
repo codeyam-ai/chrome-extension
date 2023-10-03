233ee07b3f79b3c7e826025999d80644c2ca34bc
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-10-02 20:54:12 -0500

    fix: salt service url in prod

--- src/ui/app/components/zklogin/urls.saltService.test.ts
+++ src/ui/app/components/zklogin/urls.saltService.test.ts
@@ -2,11 +2,11 @@ import { getSaltServiceUrl } from './urls.saltService';
 
 test('getSaltServiceUrl', () => {
     expect(getSaltServiceUrl({ env: 'production' })).toBe(
-        'https://salt-service-dev-a49a2100f792.herokuapp.com'
+        'https://salt-service-staging-e74b300e2837.herokuapp.com'
     );
 
     expect(getSaltServiceUrl({ env: 'development' })).toBe(
-        'http://localhost:3005'
+        'https://salt-service-dev-a49a2100f792.herokuapp.com'
     );
     expect(getSaltServiceUrl({ env: 'test' })).toBe('http://localhost:3005');
     expect(getSaltServiceUrl()).toBe('http://localhost:3005');
--- src/ui/app/components/zklogin/urls.saltService.ts
+++ src/ui/app/components/zklogin/urls.saltService.ts
@@ -1,5 +1,7 @@
 type EnvType = typeof process.env.NODE_ENV | 'test';
 
+const ETHOS_SALT_SERVICE_STAGING_URL =
+    'https://salt-service-staging-e74b300e2837.herokuapp.com';
 const ETHOS_SALT_SERVICE_DEV_URL =
     'https://salt-service-dev-a49a2100f792.herokuapp.com';
 const ETHOS_SALT_SERVICE_LOCAL_URL = 'http://localhost:3005';
@@ -9,8 +11,9 @@ export function getSaltServiceUrl(
 ) {
     switch (env) {
         case 'production':
-            return ETHOS_SALT_SERVICE_DEV_URL;
+            return ETHOS_SALT_SERVICE_STAGING_URL;
         case 'development':
+            return ETHOS_SALT_SERVICE_DEV_URL;
         case 'test':
         default:
             return ETHOS_SALT_SERVICE_LOCAL_URL;
